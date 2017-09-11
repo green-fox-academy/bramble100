68a73a6a20301b56a7934cced47b27440be6a090
bramble100 <arnold.barna@gmail.com>
2017-09-11 20:33:42 +0200

    new(AppendA)

---
+++ week-02/day-2/AppendA/AppendA/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppendA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create a string variable named `am` and assign the value `kuty` to it
+            // - Write a function called `appendA` that gets a string as an input
+            //   and appends an 'a' character to its end
+            // - Print the result of `appendA(am)`
+
+            string am = "kuty";
+
+            Console.WriteLine(appendA(am));
+
+            Console.ReadKey();
+        }
+
+        static string appendA(string input) => (input + 'a');
+    }
+}
