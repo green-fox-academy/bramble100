6c9ea8d92873cec4cd4d9a02b57a6e114ddd5878
bramble100 <arnold.barna@gmail.com>
2017-09-11 21:57:27 +0200

    new(DoubleItems)

---
+++ week-02/day-2/DoubleItems/DoubleItems/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubleItems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `ag`
+            //   with the following content: `[3, 4, 5, 6, 7]`
+            // - Double all the values in the array
+
+            int[] ag = new int[] { 3, 4, 5, 6, 7 };
+
+            for (int i = 0; i< ag.Length; i++)
+            {
+                ag[i] *= 2;
+            }
+
+            foreach (int i in ag)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
