9b119f850ef8e4996d1d8122ea9104ac6401caa8
bramble100 <arnold.barna@gmail.com>
2017-09-11 20:46:26 +0200

    new(Factorio)

---
+++ week-02/day-2/Factorio/Factorio/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create a function called `factorio`
+            //   that returns it's input's factorial
+
+            Console.WriteLine("Enter an integer:");
+
+            Console.WriteLine("Factorio is: " + factorio(Int32.Parse(Console.ReadLine())));
+
+            Console.ReadKey();
+        }
+
+        static long factorio(int number) => (number == 1 ? 1 : number * factorio(number - 1));
+    }
+}
---
+++ week-02/day-2/Sum/Sum/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            // - Write a function called `sum` that sum all the numbers
+            //   until the given parameter
+
+
+        }
+    }
+}
