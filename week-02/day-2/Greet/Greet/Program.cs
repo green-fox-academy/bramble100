d4515b9e1f0478b885fdbc2279b57035dd0aaaa4
bramble100 <arnold.barna@gmail.com>
2017-09-11 20:31:05 +0200

    new(Greet)

---
+++ week-02/day-2/Greet/Greet/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create a string variable named `al` and assign the value `Greenfox` to it
+            // - Create a function called `greet` that greets it's input parameter
+            //     - Greeting is printing e.g. `Greetings dear, Greenfox`
+            // - Greet `al`
+
+            string a1 = "Greenfox";
+
+            Console.WriteLine(greet(a1));
+
+            Console.ReadKey();
+        }
+
+        static string greet(string name) => ("Greetings dear, " + name);
+    }
+}
