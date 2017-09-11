334e1069433d615d01d73c08192c30687764e65e
bramble100 <arnold.barna@gmail.com>
2017-09-11 20:18:48 +0200

    new(Third)

--- week-02/BetValidator/BetValidator/Program.cs
+++ week-02/BetValidator/BetValidator/Program.cs
@@ -12,40 +12,45 @@ namespace BetValidator
     {
         static void Main(string[] args)
         {
-            string textMessage = "12 90 3 44 2";
+            string textMessage = "12 90, 3,,,; 44 2";
             List<long> gameType = new List<long> { 5, 90 };
 
+            Console.WriteLine("SMS Lottery Bet Validator");
+            Console.WriteLine("The user has to guess {0} numbers out of {1}.", gameType[0], gameType[1]);
             Console.WriteLine("The user input for this game type is: " + textMessage);
+            Console.WriteLine("The text message for this game type is {0}.", TextMessageIsValid(textMessage) ? "valid" : "invalid");
 
             SortedSet<long> userInput = ProcessedUserInput(textMessage);
             Console.WriteLine("The user input for this game type is {0}.", BetIsValid(gameType, userInput) ? "valid" : "invalid");
 
             if (BetIsValid(gameType, userInput))
             {
+                Console.WriteLine("The cleaned and sorted user input is:");
                 foreach(long bet in userInput)
                 {
-                    Console.WriteLine(bet);
+                    Console.Write("{0} ", bet);
                 }
-                
             }
-            
-
             Console.ReadKey();
         }
 
+        static bool TextMessageIsValid(string userInput)
+        {
+            return true;
+        }
         static SortedSet<long> ProcessedUserInput(string userInput)
         {
             SortedSet<long> returnList = new SortedSet<long>();
             foreach (string segment in userInput.Split(' ', ','))
             {
-                returnList.Add(Int64.Parse(segment));
+                if (Int64.TryParse(segment, out long result))
+                {
+                    returnList.Add(result);
+                }
             }
             return returnList;
         }
 
-        static bool BetIsValid(List<long> gameType, SortedSet<long> bet)
-        {
-            return (bet.Count == gameType[0] && bet.Max <= gameType[1]);
-        }
+        static bool BetIsValid(List<long> gameType, SortedSet<long> bet) => (bet.Count == gameType[0] && bet.Max <= gameType[1]);
     }
 }
