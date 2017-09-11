0f6d6848a2dbb1f8495c32b503fd33eb188f0a87
bramble100 <arnold.barna@gmail.com>
2017-09-11 22:05:53 +0200

    new(AppendAArray)

---
+++ week-02/day-2/AppendAArray/AppendAArray/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppendAArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `nimals`
+            //   with the following content: `["kuty", "macs", "cic"]`
+            // - Add all elements an `"a"` at the end
+
+            string[] nimals = new string[] { "kuty", "macs", "cic" };
+
+            for(int i = 0; i< nimals.Length; i++)
+            {
+                nimals[i] += 'a';
+            }
+
+            foreach(string s in nimals)
+            {
+                Console.WriteLine(s);
+            }
+            Console.ReadKey();
+        }
+    }
+}
