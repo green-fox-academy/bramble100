f396a4b391c5515bc1f296bf3738c55d040d03c7
bramble100 <arnold.barna@gmail.com>
2017-09-11 21:27:42 +0200

    new(IncrementElement)

---
+++ week-02/day-2/IncrementElement/IncrementElement/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncrementElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //- Create an array variable named `t`
+            //  with the following content: `[1, 2, 3, 4, 5]`
+            //- Increment the third element
+            //- Print the third element
+            int[] t = new int[] { 1, 2, 3, 4, 5 };
+
+            t[2]++;
+            Console.WriteLine(t[2]);
+
+            Console.ReadKey();
+        }
+    }
+}
