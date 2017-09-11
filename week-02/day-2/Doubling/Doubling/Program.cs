c359c7268540efe8a26876f28957c433991f883d
bramble100 <arnold.barna@gmail.com>
2017-09-11 20:27:39 +0200

    new(Doubling)

---
+++ week-02/day-2/Doubling/Doubling/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doubling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an integer variable named `ak` and assign the value `123` to it
+            // - Create a function called `doubling` that doubles it's input parameter
+            // - Print the result of `doubling(ak)`
+
+            int ak = 123;
+
+            Console.WriteLine(doubling(ak));
+
+            Console.ReadKey();
+        }
+
+        static int doubling(int number) => number * 2;
+    }
+}
