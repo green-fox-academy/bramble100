cab80a1ce7f73ce64b73e62ab56e25f4cfd838fc
bramble100 <arnold.barna@gmail.com>
2017-09-11 13:27:56 +0200

    AnagramDeterminer solved

---
+++ week-02/anagram/Anagram/Anagram/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anagram
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first string:");
+            string string1 = Console.ReadLine();
+            Console.WriteLine("Enter the second string:");
+            string string2 = Console.ReadLine();
+
+            Hashtable lettersInString1 = new Hashtable();
+            bool theyAreAnagrams = true;
+
+            foreach (char character in string1)
+            {
+                lettersInString1[character] = lettersInString1.ContainsKey(character) ? lettersInString1[character] : 1;
+            }
+
+            foreach(char character in string2)
+            {
+                if(lettersInString1.ContainsKey(character))
+                {
+                    lettersInString1[character] = Int32.Parse(lettersInString1[character].ToString()) - 1;
+                }
+                else
+                {
+                    theyAreAnagrams = false;
+                    break;
+                }
+            }
+
+            Console.WriteLine("The first string is \"{0}\", the second string is \"{1}\", and they are {2}anagrams.",
+                string1,
+                string2,
+                theyAreAnagrams? "":"not ");
+
+            Console.ReadKey();
+        }
+    }
+}
