2bf1302d37c65b3b75740e648cf061ea5e9139e2
bramble100 <arnold.barna@gmail.com>
2017-09-12 21:26:37 +0200

    new(AppendLetter)

---
+++ week-02/day-3/AppendLetter/AppendLetter/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppendLetter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> far = new List<string> { "kuty", "macsk", "kacs", "rók", "halacsk" };
+            // Add "a" to every string in the far list.
+
+            for (int i = 0; i < far.Count; i++)
+            {
+                far[i] = far.ElementAt(i) + "a";
+            }
+            foreach (string s in far)
+            {
+                Console.WriteLine(s);
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
