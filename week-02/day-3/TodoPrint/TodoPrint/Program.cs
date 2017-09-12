2099b895eef8709136fec4ddf9d8c5cb0ed4760b
bramble100 <arnold.barna@gmail.com>
2017-09-12 20:58:51 +0200

    new(TodoPrint)

---
+++ week-02/day-3/TodoPrint/TodoPrint/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoPrint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string todoText = " - Buy milk\n";
+            // Add "My todo:" to the beginning of the todoText
+            // Add " - Download games" to the end of the todoText
+            // Add " - Diablo" to the end of the todoText but with indention
+
+            // Expected output:
+
+            // My todo:
+            //  - Buy milk
+            //  - Download games
+            //      - Diablo
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("My todo:");
+            sb.Append(todoText);
+            sb.AppendLine(" - Download games");
+            sb.AppendLine("     - Diablo");
+
+            todoText = sb.ToString();
+
+            Console.WriteLine(todoText);
+            Console.ReadKey();
+        }
+    }
+}
