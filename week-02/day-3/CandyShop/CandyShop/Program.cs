314e3405922c69720bb85443a657d68afceef360
bramble100 <arnold.barna@gmail.com>
2017-09-12 21:30:26 +0200

    new(CandyShop)

---
+++ week-02/day-3/CandyShop/CandyShop/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CandyShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> list = new List<object>();
+            list.Add("Cupcake");
+            list.Add(2);
+            list.Add("Brownie");
+            list.Add(false);
+
+            // Accidentally we added "2" and "false" to the list. 
+            // Your task is to change from "2" to "Croissant" and change from "false" to "Ice cream"
+            // No, don't just remove the lines
+
+            list[list.IndexOf(2)] = "Croissant";
+            list[list.IndexOf(false)] = "Ice cream";
+
+            foreach (object o in list)
+            {
+                Console.WriteLine(o);
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
