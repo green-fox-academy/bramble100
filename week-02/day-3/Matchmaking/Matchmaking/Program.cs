c48c63a6342779a33d7f5deba44ff0d39a4f9c85
bramble100 <arnold.barna@gmail.com>
2017-09-12 21:22:32 +0200

    new(Matchmaking)

---
+++ week-02/day-3/Matchmaking/Matchmaking/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matchmaking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var girls = new List<string> { "Eve", "Ashley", "Bözsi", "Kat", "Jane" };
+            var boys = new List<string> { "Joe", "Fred", "Béla", "Todd", "Neef", "Jeff" };
+            var order = new List<string>();
+
+            // Join the two lists by matching one girl with one boy in the order list
+            // Exepected output: "Eve", "Joe", "Ashley", "Fred"...
+
+            for (int i = 0; i < girls.Count; i++)
+            {
+                order.Add(girls.ElementAt(i));
+                order.Add(boys.ElementAt(i));
+            }
+            foreach (string s in order)
+            {
+                Console.WriteLine(s);
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
