5e2d542a1210d2e144f581696884145d34c6642b
bramble100 <arnold.barna@gmail.com>
2017-09-12 08:05:18 +0200

    improve(BetValidator)

--- week-02/BetValidator/BetValidator/Program.cs
+++ week-02/BetValidator/BetValidator/Program.cs
@@ -12,30 +12,42 @@ namespace BetValidator
     {
         static void Main(string[] args)
         {
-            string textMessage = "12 90, 3,,,; 44 2";
+            string textMessage = "12 90, 3,,, 44 2";
             List<long> gameType = new List<long> { 5, 90 };
 
             Console.WriteLine("SMS Lottery Bet Validator");
             Console.WriteLine("The user has to guess {0} numbers out of {1}.", gameType[0], gameType[1]);
             Console.WriteLine("The user input for this game type is: " + textMessage);
-            Console.WriteLine("The text message for this game type is {0}.", TextMessageIsValid(textMessage) ? "valid" : "invalid");
 
-            SortedSet<long> userInput = ProcessedUserInput(textMessage);
-            Console.WriteLine("The user input for this game type is {0}.", BetIsValid(gameType, userInput) ? "valid" : "invalid");
-
-            if (BetIsValid(gameType, userInput))
+            if (TextMessageIsValid(textMessage))
             {
+                Console.WriteLine("The text message for this game type is valid.");
+                SortedSet<long> userInput = ProcessedUserInput(textMessage);
                 Console.WriteLine("The cleaned and sorted user input is:");
-                foreach(long bet in userInput)
+                foreach (long bet in userInput)
                 {
                     Console.Write("{0} ", bet);
                 }
+
+                Console.WriteLine("The cleaned used input for this game type is {0}.", BetIsValid(gameType, userInput) ? "valid" : "invalid");
             }
+            else
+            {
+                Console.WriteLine("The text message for this game type is invalid.");
+            }
+
             Console.ReadKey();
         }
 
         static bool TextMessageIsValid(string userInput)
         {
+            foreach(char c in userInput)
+            {
+                if (!Byte.TryParse(c.ToString(), out byte result) && !c.ToString().Contains(",") && !c.ToString().Contains(" "))
+                {
+                    return false;
+                }
+            }
             return true;
         }
         static SortedSet<long> ProcessedUserInput(string userInput)
