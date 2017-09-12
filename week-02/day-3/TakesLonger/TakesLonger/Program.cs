c7be7d30f9afa5fe61a466f8eacd8010695bdc54
bramble100 <arnold.barna@gmail.com>
2017-09-12 20:49:49 +0200

    new(TakesLonger)

---
+++ week-02/day-3/TakesLonger/TakesLonger/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TakesLonger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string quote = "Hofstadter's Law: It you expect, even when you take into account Hofstadter's Law.";
+
+            // When saving this quote a disk error has occured. Please fix it.
+            // Add "always takes longer than" to the StringBuilder (quote) between the words "It" and "you"
+            // Using pieces of the quote variable (instead of just redefining the string)
+
+            quote = quote.Replace("It you", "It always takes longer than you");
+
+            Console.WriteLine(quote);
+            Console.ReadKey();
+        }
+    }
+}
