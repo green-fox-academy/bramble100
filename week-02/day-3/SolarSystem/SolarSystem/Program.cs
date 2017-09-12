a66d5ca36c5e096e276b7e49a5ae584ae922b6dd
bramble100 <arnold.barna@gmail.com>
2017-09-12 21:15:07 +0200

    new(SolarSystem)

---
+++ week-02/day-3/SolarSystem/SolarSystem/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolarSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> planetList = new List<string> { "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus",
+            "Neptune" };
+
+
+            // Saturn is missing from the planetList
+            // Insert it into the correct position
+
+            planetList.Insert(5, "Saturn");
+
+            foreach (string s in planetList)
+            {
+                Console.WriteLine(s);
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
