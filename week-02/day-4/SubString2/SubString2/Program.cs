ab6eae5b5abefea5ce048b4788431def15e4b1ce
bramble100 <arnold.barna@gmail.com>
2017-09-13 16:54:38 +0200

    new(SubString2)

---
+++ week-02/day-4/SubString2/SubString2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubString2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> string1 = new List<string>() { "this", "is", "what", "I'm", "searching", "in" };
+            string string2 = "searching";
+
+            Console.WriteLine(MyStringFinder(string1, string2));
+            Console.ReadKey();
+        }
+
+        private static int MyStringFinder(List<string> mylist, string string2)
+        {
+            for (int i = 0; i < mylist.Count; i++)
+            {
+                if (mylist[i].Contains(string2)) return i;
+            }
+            return -1;
+        }
+    }
+}
