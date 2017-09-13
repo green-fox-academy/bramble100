6bbfa75063a036fab95a861588abe1bf10f98701
bramble100 <arnold.barna@gmail.com>
2017-09-13 16:39:04 +0200

    new(Substring)

---
+++ week-02/day-4/Substring/Substring/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string string1 = "this is what I'm searching in";
+            string string2 = "searching";
+
+            Console.WriteLine(MyStringFinder(string1, string2));
+            Console.ReadKey();
+        }
+
+        private static int MyStringFinder(string string1, string string2)
+        {
+            for (int i = 0; i < string1.Length-string2.Length; i++)
+            {
+                if (string1.Substring(i).StartsWith(string2))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
