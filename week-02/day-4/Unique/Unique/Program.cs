6b470c3e700fdeca6fce7154f80e2fb164853340
bramble100 <arnold.barna@gmail.com>
2017-09-13 16:11:24 +0200

    new(Unique)

---
+++ week-02/day-4/Unique/Unique/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int> { 1, 11, 34, 11, 52, 61, 1, 34 };
+            foreach (int item in new HashSet<int>(list))
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
