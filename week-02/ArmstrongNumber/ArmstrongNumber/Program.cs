476a60cb59263733782fd9ea6bf81c00a36407cf
bramble100 <arnold.barna@gmail.com>
2017-09-15 10:27:24 +0200

    new(IsArmstrongNumber)

---
+++ week-02/ArmstrongNumber/ArmstrongNumber/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArmstrongNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 1634;
+            Console.WriteLine("{0} is {1}an Armstrong number.", number, IsArmstrongNumber(number) ? "" : "NOT ");
+            number = 153;
+            Console.WriteLine("{0} is {1}an Armstrong number.", number, IsArmstrongNumber(number) ? "" : "NOT ");
+            Console.ReadKey();
+        }
+
+        private static bool IsArmstrongNumber(int number)
+        {
+            int numberOfDigits = number.ToString().Length;
+            int result = 0;
+            int actualTenDivider = (int)Math.Pow(10, numberOfDigits-1);
+            int originalNumber = number;
+            int actualDigit;
+            for (int i = 0; i < numberOfDigits; i++)
+            {
+                actualDigit = (number / actualTenDivider);
+                result += (int)Math.Pow(actualDigit, numberOfDigits);
+                number %= actualTenDivider;
+                actualTenDivider /= 10;
+            }
+            return result == originalNumber;
+        }
+    }
+}
