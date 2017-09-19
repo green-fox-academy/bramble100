de20f1f95dabba010a23d9b06296aba3ed67a772
bramble100 <arnold.barna@gmail.com>
2017-09-19 13:15:24 +0200

    new(WriteSingleLine)

---
+++ week-03/day-2/WriteSingleLine/WriteSingleLine/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WriteSingleLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Open a file called "my-file.txt"
+            // Write your name in it as a single line
+            // If the program is unable to write the file,
+            // then it should print an error message like: "Unable to write file: my-file.txt"
+            string path = @"./my-file.txt";
+            using (StreamWriter writer = new StreamWriter(path))
+            {
+                try
+                {
+                    writer.WriteLine("Arnold Barna is the fox!");
+                }
+                catch (IOException)
+                {
+                    Console.WriteLine("Unable to write file: my-file.txt"); ;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
