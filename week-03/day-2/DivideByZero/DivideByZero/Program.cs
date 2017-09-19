0202feb10abb432ad735ae6521b705ac351bc48c
bramble100 <arnold.barna@gmail.com>
2017-09-19 13:10:40 +0200

    new(DivideByZero)

---
+++ week-03/day-2/DivideByZero/DivideByZero/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivideByZero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create a function that takes a number
+            // divides ten with it,
+            // and prints the result.
+            // it should print "fail" if the parameter is 0
+            Console.WriteLine(Divide(2,0));
+            Console.ReadKey();
+        }
+
+        static double Divide(double nominator, double divider)
+        {
+            try
+            {
+                int result = (int)nominator / (int)divider;
+                //double result = nominator / divider;
+                return (double)result;
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Divide by zero :(");
+                return 0;
+            }
+        }
+    }
+}
