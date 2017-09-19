1fab3955c568d3ab444802e5599761cad3f0f343
bramble100 <arnold.barna@gmail.com>
2017-09-19 15:57:46 +0200

    new(ReverseOrder)

---
+++ week-03/day-2/ReversedOrder/ReversedOrder/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReversedOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a method that decrypts reversed-lines.txt
+            string path = @"./reversed-order.txt";
+            foreach (string line in ReverseOrder(path))
+            {
+                Console.WriteLine(line);
+            }
+            Console.ReadKey();
+        }
+
+        private static string[] ReverseOrder(string path)
+        {
+            string[] fileContent = { };
+            List<string> reverseOrderText = new List<string>();
+            try
+            {
+                fileContent = File.ReadAllLines(path);
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("Read error");
+            }
+
+            for (int i = fileContent.Length-1; i >= 0; i--)
+            {
+                reverseOrderText.Add(fileContent[i]);
+            }
+            return reverseOrderText.ToArray<string>();
+        }
+
+    }
+}
