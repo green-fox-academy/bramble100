9f028d76fe7394ee176b393c2d6eeed8a4a5e4d4
bramble100 <arnold.barna@gmail.com>
2017-09-19 16:09:40 +0200

    new(EncodedLines)

---
+++ week-03/day-2/EncodedLines/EncodedLines/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncodedLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a method that decrypts encoded-lines.txt
+            string path = @"./encoded-lines.txt";
+            foreach (string line in EncodeFile(path))
+            {
+                Console.WriteLine(line);
+            }
+            Console.ReadKey();
+        }
+
+        private static string[] EncodeFile(string path)
+        {
+            string[] fileContent = { };
+            List<string> encodedText = new List<string>();
+            try
+            {
+                fileContent = File.ReadAllLines(path);
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("Read error");
+            }
+
+            foreach (string line in fileContent)
+            {
+                string encodedLine = "";
+                for (int i = 0; i < line.Length; i++)
+                {
+                    encodedLine += line[i] == ' ' ? ' ' : Convert.ToChar(line[i] - 1);
+                }
+                encodedText.Add(encodedLine);
+            }
+            return encodedText.ToArray<string>();
+        }
+    }
+}
