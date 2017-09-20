e6c93acd1e3e9d7067fb2fdc642d700793ba1e93
bramble100 <arnold.barna@gmail.com>
2017-09-20 13:49:37 +0200

    new(FourRectangles)

--- week-03/day-3/FourRectangles/FourRectangles/MainWindow.xaml.cs
+++ week-03/day-3/FourRectangles/FourRectangles/MainWindow.xaml.cs
@@ -12,17 +12,24 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using GreenFox;
 namespace FourRectangles
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            // draw four different size and color rectangles.
+
+            Random r = new Random();
+
+            for (int i = 0; i < 4; i++)
+            {
+                foxDraw.FillColor(Color.FromArgb((byte)r.Next(255), (byte)r.Next(255), (byte)r.Next(255), (byte)r.Next(255)));
+                foxDraw.DrawRectangle(r.Next(300), r.Next(300), r.Next(100), r.Next(100));
+            }
         }
     }
 }
