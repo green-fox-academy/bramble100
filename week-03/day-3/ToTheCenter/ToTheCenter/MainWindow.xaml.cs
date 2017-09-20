b17c633281f9999ed1ea1dda4f55c315589ca1e8
bramble100 <arnold.barna@gmail.com>
2017-09-20 11:49:11 +0200

    new(ToTheCenter)

---
+++ week-03/day-3/ToTheCenter/ToTheCenter/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace ToTheCenter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            // create a line drawing function that takes 2 parameters:
+            // the x and y coordinates of the line's starting point
+            // and draws a line from that point to the center of the canvas.
+            // draw 3 lines with that function.
+
+            DrawLine(10, 10);
+            DrawLine(200, 20);
+            DrawLine(100, 234);
+
+            void DrawLine(int x, int y)
+            {
+                foxDraw.StrokeColor(Colors.Green);
+                foxDraw.DrawLine(new Point(x,y), new Point(canvas.Width/2, canvas.Height/2));
+            }
+
+        }
+    }
+}
