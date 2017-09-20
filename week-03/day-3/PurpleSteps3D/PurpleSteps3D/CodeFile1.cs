03237ea4ef80f8378a14eae936949b93782a5554
bramble100 <arnold.barna@gmail.com>
2017-09-20 16:21:58 +0200

    PurpleSteps3D

---
+++ week-03/day-3/PurpleSteps3D/PurpleSteps3D/CodeFile1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+
+namespace GreenFox
+{
+    public static class Helper
+    {
+        public static Color RandomColor(Random random)
+        {
+            return Color.FromArgb((byte)random.Next(),
+                (byte)random.Next(),
+                (byte)random.Next(),
+                (byte)random.Next());
+        }
+    }
+}
---
+++ week-03/day-3/PurpleSteps3D/PurpleSteps3D/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace PurpleSteps3D
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            Random random = new Random();
+            int pos = 10;
+            for (int size = pos; size < 70; size += 10)
+            {
+                DrawRectangle(new Point(pos, pos), size , Helper.RandomColor(random), foxDraw);
+                pos += size;
+            }
+        }
+
+        public void DrawRectangle(Point point, int size, Color fillcolor, FoxDraw foxDraw)
+        {
+            foxDraw.FillColor(fillcolor);
+            foxDraw.DrawRectangle(point.X, point.Y, size, size);
+        }
+
+
+    }
+}
