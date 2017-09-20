7759a058221a21ecf05d6a6fac33e6efd2bfc2ab
bramble100 <arnold.barna@gmail.com>
2017-09-20 17:23:00 +0200

    Checkerboard

---
+++ week-03/day-3/Checkerboard/Checkerboard/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace Checkerboard
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+
+            int fieldSize = 30;
+            int numberOfFieldsInEdge = 8;
+            for (int i = 0; i < numberOfFieldsInEdge; i++)
+            {
+                for (int j = 0; j < numberOfFieldsInEdge; j++)
+                {
+                    foxDraw.FillColor((i + j) % 2 == 0 ? Colors.Red : Colors.Pink);
+                    foxDraw.DrawRectangle(fieldSize * j, fieldSize * i, fieldSize, fieldSize);
+                }
+            }
+        }
+    }
+}
