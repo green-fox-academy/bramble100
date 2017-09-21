31b9d6035dcc74563eae8e821f9bc6f829775a22
bramble100 <arnold.barna@gmail.com>
2017-09-21 17:28:14 +0200

    improve: List changed to Queue

--- week-02/JosephusProblem/JosephusProblem/Program.cs
+++ week-02/JosephusProblem/JosephusProblem/Program.cs
@@ -12,20 +12,31 @@ namespace JosephusProblem
         {
             for (int i = 3; i < 42; i++)
             {
-                Console.WriteLine("{0}: {1}", i, FindSurvivorSeat(i));
+                Console.WriteLine("{0}: {1}", i, FindSurvivorSeat_WithQueue(i));
             }
             Console.ReadKey();
         }
 
-        private static int FindSurvivorSeat(int numberOfParticipants)
+        private static int FindSurvivorSeat_WithList(int numberOfParticipants)
         {
             // value: original pos
             var gamers = new List<int>(Enumerable.Range(1, numberOfParticipants));
             while (gamers.Count > 1)
             {
-                gamers.RemoveAt(1);
-                gamers.Add(gamers.First());
-                gamers.RemoveAt(0);
+                gamers.RemoveAt(1); // second gets killed
+                gamers.Add(gamers.First()); // first goes at the end
+                gamers.RemoveAt(0); // first goes at the end
+            }
+            return gamers.First();
+        }
+
+        private static int FindSurvivorSeat_WithQueue(int numberOfParticipants)
+        {
+            var gamers = new Queue<int>(Enumerable.Range(1, numberOfParticipants));
+            while (gamers.Count > 1)
+            {
+                gamers.Enqueue(gamers.Dequeue()); // first goes at the end
+                gamers.Dequeue(); // second gets killed
             }
             return gamers.First();
         }
