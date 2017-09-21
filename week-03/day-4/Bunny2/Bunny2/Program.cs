c2e966fcf14d529117bc3b38b5d9e5b78549f070
bramble100 <arnold.barna@gmail.com>
2017-09-21 12:41:06 +0200

    new(Bunny2)

---
+++ week-03/day-4/Bunny2/Bunny2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bunny2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // We have bunnies standing in a line, numbered 1, 2, ... The odd bunnies
+            // (1, 3, ..) have the normal 2 ears. The even bunnies (2, 4, ..) we'll say
+            // have 3 ears, because they each have a raised foot. Recursively return the
+            // number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
+            for (int numberOfBunnies = 1; numberOfBunnies < 51; numberOfBunnies++)
+            {
+                Console.WriteLine($"{numberOfBunnies}: { NumberOfEars(numberOfBunnies)}");
+            }
+            Console.ReadKey();
+
+        }
+
+        private static int NumberOfEars(int numberOfBunnies)
+        {
+            if (numberOfBunnies < 2)
+            {
+                return 2;
+            }
+            return (numberOfBunnies % 2 == 0 ? 3 : 2) + NumberOfEars(numberOfBunnies - 1);
+        }
+    }
+}
