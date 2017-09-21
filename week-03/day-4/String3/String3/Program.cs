e192bb9fa499f45b9244b40559657e9e51e74a7d
bramble100 <arnold.barna@gmail.com>
2017-09-21 10:47:55 +0200

    new(String3)

---
+++ week-03/day-4/String3/String3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Given a string, compute recursively a new string where all the
+            // adjacent chars are now separated by a "*".
+            string input = "Green Fox";
+            Console.WriteLine(Asteriskize(input));
+            Console.ReadKey();
+        }
+
+        private static string Asteriskize(string input) => 
+            input.Length < 2 ? input : $"{input[0]}*{Asteriskize(input.Substring(1))}";
+    }
+}
