9cdde018a185f50778f500c30f959e8d4d8c251a
bramble100 <arnold.barna@gmail.com>
2017-09-22 15:58:09 +0200

    new(KochLine)

---
+++ week-03/day-5/KochLine/KochLine/Helper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using GreenFox;
+using System.Collections.Generic;
+
+namespace GreenFox
+{
+    public static class Helper
+    {
+        public static Color RandomColor(Random random)
+        {
+            return Color.FromArgb((byte)random.Next(),
+                (byte)random.Next(),
+                (byte)random.Next(),
+                (byte)random.Next());
+        }
+
+        public static void DrawHexagon(int xPosition, 
+            int yPosition, 
+            int edge, 
+            Color lineColor,
+            Color shapeColor,
+            FoxDraw foxDraw)
+        {
+            foxDraw.FillColor(shapeColor);
+            foxDraw.StrokeColor(lineColor);
+            foxDraw.DrawPolygon(new List<Point>
+            {
+                new Point(xPosition - edge / 2, yPosition - edge * 0.866), // 0
+                new Point(xPosition + edge / 2, yPosition - edge * 0.866), // 1
+                new Point(xPosition + edge, yPosition), // 2
+                new Point(xPosition + edge / 2, yPosition + edge * 0.866), // 3
+                new Point(xPosition - edge / 2, yPosition + edge * 0.866), // 4
+                new Point(xPosition - edge, yPosition), // 5
+                new Point(xPosition - edge / 2, yPosition - edge * 0.866) // 0
+            });
+        }
+
+        internal static Point CalculatePointOffset(Point startPoint,
+            double length,
+            double directionInDegrees)
+        {
+            double directionInRadian = Math.PI * directionInDegrees / 180.0;
+            return new Point(startPoint.X + length * Math.Cos(directionInRadian),
+                startPoint.Y + length * Math.Sin(directionInRadian));
+        }
+    }
+}
---
+++ week-03/day-5/KochLine/KochLine/KochLineDrawer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GreenFox;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows;
+
+namespace KochLine
+{
+    class KochLineDrawer
+    {
+        FoxDraw foxDraw;
+        Canvas canvas;
+
+        public KochLineDrawer(FoxDraw foxDraw, Canvas canvas)
+        {
+            this.foxDraw = foxDraw;
+            this.canvas = canvas;
+
+            foxDraw.FillColor(Colors.FloralWhite);
+            foxDraw.StrokeColor(Colors.FloralWhite);
+            foxDraw.DrawRectangle(0, 0, canvas.Width, canvas.Height);
+        }
+
+        public void Draw(Point startPoint, 
+            double direction,
+            double length,
+            int depth)
+        {
+            foxDraw.FillColor(Colors.Firebrick);
+            foxDraw.StrokeColor(Colors.Firebrick);
+            Point endPoint = Helper.CalculatePointOffset(startPoint, length, direction);
+            if(depth <= 0)
+            {
+                foxDraw.DrawLine(startPoint, endPoint);
+                return;
+            }
+            Point breakPoint1 = Helper.CalculatePointOffset(startPoint, length / 3.0, direction);
+            Point breakPoint2 = Helper.CalculatePointOffset(breakPoint1, length / 3.0, direction - 60);
+            Point breakPoint3 = Helper.CalculatePointOffset(breakPoint2, length / 3.0, direction + 60);
+            Draw(startPoint, direction, length / 3.0, depth - 1);
+            Draw(breakPoint1, direction - 60, length / 3.0, depth - 1);
+            Draw(breakPoint2, direction + 60, length / 3.0, depth - 1);
+            Draw(breakPoint3, direction, length / 3.0, depth - 1);
+        }
+    }
+}
