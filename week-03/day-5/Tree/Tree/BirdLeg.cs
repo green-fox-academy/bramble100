682ecc8ecf40ad0b6fa23d648be63f085e47ee5e
bramble100 <arnold.barna@gmail.com>
2017-09-22 09:59:33 +0200

    new(Tree)

---
+++ week-03/day-5/Tree/Tree/BirdLeg.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GreenFox;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Controls;
+
+namespace Tree
+{
+    public class BirdLeg
+    {
+        FoxDraw foxDraw;
+        Random random;
+        bool isRandom = false;
+        bool isRandomColored = false;
+        int numberOfToes = 3;
+
+        public BirdLeg(FoxDraw foxDraw,
+            Random random,
+            Canvas canvas,
+            bool isRandom = false, 
+            bool isRandomColored = false, 
+            int numberOfToes = 3)
+        {
+            this.foxDraw = foxDraw;
+            this.random = random;
+            this.isRandom = isRandom;
+            this.numberOfToes = numberOfToes;
+            this.isRandomColored = false;
+            foxDraw.FillColor(Colors.Aqua);
+            foxDraw.StrokeColor(Colors.Aqua);
+            foxDraw.DrawRectangle(0, 0, canvas.Width, canvas.Height);
+        }
+
+        internal void Draw(Point startPoint, 
+            int length = 100, 
+            int depth = 1)
+        {
+            foxDraw.FillColor(Colors.Red);
+
+        }
+    }
+}
---
+++ week-03/day-5/Tree/Tree/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace Tree
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            BirdLeg birdleg = new BirdLeg(new FoxDraw(canvas), new Random(), canvas);
+            birdleg.Draw(new Point(canvas.Width / 2, canvas.Height));
+        }
+    }
+}
