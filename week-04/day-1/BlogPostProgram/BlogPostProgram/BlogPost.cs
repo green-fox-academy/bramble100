deb75da3023a9d9d0cff86cd2a40d6466ebf636c
bramble100 <arnold.barna@gmail.com>
2017-09-25 13:25:48 +0200

    new(BlogPost)

---
+++ week-04/day-1/BlogPostProgram/BlogPostProgram/BlogPost.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogPostProgram
+{
+    class BlogPost
+    {
+        public string AuthorName;
+        public string Title;
+        public string Text;
+        public DateTime PublicationDate;
+
+        public BlogPost(string authorName, string title, string text, DateTime publicationDate)
+        {
+            AuthorName = authorName;
+            Title = title;
+            Text = text;
+            PublicationDate = publicationDate;
+        }
+    }
+
+
+}
