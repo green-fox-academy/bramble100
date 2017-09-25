8a4ab76d6fc0bb176ffde2d308eab846c956c1e8
bramble100 <arnold.barna@gmail.com>
2017-09-25 11:48:48 +0200

    new(FiveTrees)

---
+++ week-04/day-1/FiveTrees/FiveTrees/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiveTrees
+{
+    //Create 5 trees
+    //Store the data of them in variables in your program
+    //for every tree the program should store its'
+    // type
+    // leaf color
+    // age
+    // sex
+    //you can use just variables, or lists and/or maps
+
+    class Program
+    {
+        public struct Tree
+        {
+            string Type;
+            string LeafColor;
+            int Age;
+            string Sex;
+        }
+
+        static void Main(string[] args)
+        {
+            Tree[] trees = new Tree[5];
+            Console.ReadKey();
+        }
+    }
+}
