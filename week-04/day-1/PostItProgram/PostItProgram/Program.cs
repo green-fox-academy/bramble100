a1b4334bb0130c9bbbc37a78835c3292f1906072
bramble100 <arnold.barna@gmail.com>
2017-09-25 16:16:37 +0200

    new(PostItProgram)

---
+++ week-04/day-1/PostItProgram/PostItProgram/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostItProgram
+{
+    public struct PostIt
+    {
+        public string Text;
+        public string BackgroundColor;
+        public string TextColor;
+
+        public PostIt(string text, string backgroundColor, string textColor)
+        {
+            Text = text ?? throw new ArgumentNullException(nameof(text));
+            BackgroundColor = backgroundColor ?? throw new ArgumentNullException(nameof(backgroundColor));
+            TextColor = textColor ?? throw new ArgumentNullException(nameof(textColor));
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PostIt[] postIts = new PostIt[3];
+            //List<PostIt> postItsList = new List<PostIt>;
+
+            postIts[0] = new PostIt("Idea 1", "orange", "blue");
+            postIts[1] = new PostIt("Awesome", "pink", "black");
+            postIts[2] = new PostIt("Superb!", "yellow", "green");
+        }
+    }
+}
