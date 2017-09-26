34d9c5de4f2a33683050b539e8d380a15051ca2a
bramble100 <arnold.barna@gmail.com>
2017-09-26 08:00:20 +0200

    new(Animal)

---
+++ week-04/day-1/AnimalProgram/AnimalProgram/Animal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalProgram
+{
+    class Animal
+    {
+        private int hunger = 50;
+        private int thirst = 50;
+
+        public void Eat() => hunger -= 1;
+
+        public void Drink() => thirst -= 1;
+
+        public void Play()
+        {
+            hunger += 1;
+            thirst += 1;
+        }
+    }
+}
