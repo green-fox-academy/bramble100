da299b375dd3440946004b91859971f6d3a7e6f3
bramble100 <arnold.barna@gmail.com>
2017-09-26 21:21:06 +0200

    improve(Garden): changed to List<Plant>

--- week-04/day-2/GardenApp/GardenApp/Garden.cs
+++ week-04/day-2/GardenApp/GardenApp/Garden.cs
@@ -5,33 +5,27 @@ namespace GardenApp
 {
     internal class Garden : List<Plant>
     {
-        List<Plant> plants = new List<Plant>();
 
-
-
-        public Garden(List<Plant> plants) : base(plants)
-        {
-            plants.ForEach(item => plants.Add(item));
-        }
+        public Garden(List<Plant> plants) : base(plants) => plants.ForEach(item => Add(item));
 
         public Garden()
         {
         }
 
-        internal void AddFlowers(List<Flower> list) => list.ForEach(item => plants.Add(item));
+        internal void AddFlowers(List<Flower> list) => list.ForEach(item => Add(item));
 
-        internal void AddTrees(List<Tree> list) => list.ForEach(item => plants.Add(item));
+        internal void AddTrees(List<Tree> list) => list.ForEach(item => Add(item));
 
         internal void Water(int water)
         {
             Console.WriteLine($"Watering with {water}");
 
             int numberOfPlantsNeedWatering = 0;
-            plants.ForEach(item => numberOfPlantsNeedWatering += item.NeedsWatering() ? 1 : 0);
+            ForEach(item => numberOfPlantsNeedWatering += item.NeedsWatering() ? 1 : 0);
 
             int waterForOnePlant = water / numberOfPlantsNeedWatering;
 
-            plants.ForEach(item => item.Water(waterForOnePlant));
+            ForEach(item => item.Water(waterForOnePlant));
 
             Console.WriteLine();
         }
@@ -40,7 +34,7 @@ namespace GardenApp
         {
             Console.WriteLine("Garden Status Report");
             Console.WriteLine("--------------------");
-            plants.ForEach(item => Console.WriteLine(item.DisplayStatusReport()));
+            ForEach(item => Console.WriteLine(item.DisplayStatusReport()));
             Console.WriteLine();
         }
     }
