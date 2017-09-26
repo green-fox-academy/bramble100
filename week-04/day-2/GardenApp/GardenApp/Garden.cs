47d838a96a59162dceafeb6b07a4b5876851fd33
bramble100 <arnold.barna@gmail.com>
2017-09-26 14:25:03 +0200

    improve(GardenApp): seemingly works

--- week-04/day-2/GardenApp/GardenApp/Garden.cs
+++ week-04/day-2/GardenApp/GardenApp/Garden.cs
@@ -18,18 +18,28 @@ namespace GardenApp
             list.ForEach(item => trees.Add(item));
         }
 
+        internal void Water(int water)
+        {
+            Console.WriteLine($"Watering with {water}");
+
+            int numberOfPlantsNeedWatering = 0;
+            flowers.ForEach(item => numberOfPlantsNeedWatering += item.NeedsWatering() ? 1 : 0);
+            trees.ForEach(item => numberOfPlantsNeedWatering += item.NeedsWatering() ? 1 : 0);
+
+            int waterForOnePlant = water / numberOfPlantsNeedWatering;
+
+            flowers.ForEach(item => item.Water(waterForOnePlant));
+            trees.ForEach(item => item.Water(waterForOnePlant));
+
+            Console.WriteLine();
+        }
+
         internal void DisplayStatusReport()
         {
             Console.WriteLine("Garden Status Report");
             Console.WriteLine("--------------------");
             flowers.ForEach(item => Console.WriteLine(item.DisplayStatusReport()));
             trees.ForEach(item => Console.WriteLine(item.DisplayStatusReport()));
-            Console.WriteLine();
-        }
-
-        internal void Water(int water)
-        {
-            Console.WriteLine($"Watering with {water}");
             Console.WriteLine();
         }
     }
--- week-04/day-2/GardenApp/GardenApp/Tree.cs
+++ week-04/day-2/GardenApp/GardenApp/Tree.cs
@@ -12,14 +12,19 @@ namespace GardenApp
             this.color = color;
         }
 
+        public bool NeedsWatering()
+        {
+            return water < 10;
+        }
+
+        public void Water(int water)
+        {
+            this.water += (int)(water * 0.4);
+        }
+
         internal string DisplayStatusReport()
         {
             return $"The {color} tree {(NeedsWatering() ? "needs" : "doesn't need")} water";
-        }
-
-        private bool NeedsWatering()
-        {
-            return water < 10;
         }
     }
 }
