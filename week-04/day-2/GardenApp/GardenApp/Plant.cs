6ec2b603bc20e0a2b7471f40515bb53fc18356dc
bramble100 <arnold.barna@gmail.com>
2017-09-26 21:03:29 +0200

    feat(Plant): new abstract class introduced

---
+++ week-04/day-2/GardenApp/GardenApp/Plant.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GardenApp
+{
+    abstract class Plant
+    {
+        private static string name = "plant";
+
+        internal string color;
+        internal int water = 0;
+
+        abstract public bool NeedsWatering();
+
+        abstract public void Water(int water);
+
+        internal virtual string DisplayStatusReport()
+        {
+            return $"The {color} {name} {(NeedsWatering() ? "needs" : "doesn't need")} water";
+        }
+
+    }
+}
--- week-04/day-2/GardenApp/GardenApp/Tree.cs
+++ week-04/day-2/GardenApp/GardenApp/Tree.cs
@@ -2,29 +2,16 @@
 
 namespace GardenApp
 {
-    internal class Tree
+    internal class Tree : Plant
     {
-        private string color;
-        private int water = 0;
+        private static string name = "tree";
 
-        public Tree(string color)
-        {
-            this.color = color;
-        }
+        public Tree(string color) => this.color = color;
 
-        public bool NeedsWatering()
-        {
-            return water < 10;
-        }
+        public override bool NeedsWatering() => water < 10;
 
-        public void Water(int water)
-        {
-            this.water += (int)(water * 0.4);
-        }
+        public override void Water(int water) => this.water += (int)(water * 0.4);
 
-        internal string DisplayStatusReport()
-        {
-            return $"The {color} tree {(NeedsWatering() ? "needs" : "doesn't need")} water";
-        }
+        internal override string DisplayStatusReport() => $"The {color} {name} {(NeedsWatering() ? "needs" : "doesn't need")} water";
     }
 }
