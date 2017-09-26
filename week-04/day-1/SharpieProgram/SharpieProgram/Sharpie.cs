7a907b4930a55036b9a40c4e00fc0a9703aea85d
bramble100 <arnold.barna@gmail.com>
2017-09-26 08:11:11 +0200

    new(Sharpie)

---
+++ week-04/day-1/SharpieProgram/SharpieProgram/Sharpie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpieProgram
+{
+    class Sharpie
+    {
+        private string Color;
+        private float Width;
+        private int InkAmount = 100;
+
+        public Sharpie(string color, float width)
+        {
+            Color = color;
+            Width = width;
+        }
+
+        public void Use() => InkAmount--;
+    }
+}
