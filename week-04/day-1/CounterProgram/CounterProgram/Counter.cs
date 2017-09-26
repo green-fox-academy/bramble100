1a0dd28daa4909344f4c151f8f724e2ce70860f0
bramble100 <arnold.barna@gmail.com>
2017-09-26 08:17:50 +0200

    new(Counter)

---
+++ week-04/day-1/CounterProgram/CounterProgram/Counter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CounterProgram
+{
+    class Counter
+    {
+        private int Value = 0;
+
+        public Counter(int value = 0)
+        {
+            Value = value;
+        }
+
+        public void Add(int number = 1) => Value += number;
+
+        public string Get => Convert.ToString(Value);
+
+        public void Reset() => Value = 0;
+
+    }
+}
