0a62c72e327f04ffdd2d68e5976f3fc15d4ffaaa
bramble100 <arnold.barna@gmail.com>
2017-09-27 10:42:34 +0200

    new(Apples): unti test

---
+++ week-04/day-3/ApplesTest/ApplesTest/TestForApples.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplesTest
+{
+    [TestFixture]
+    class TestForApples
+    {
+        Apples apple = new Apples();
+
+        [TestCase]
+        public void AppleString()
+        {
+            Assert.AreEqual("apple", apple.GetApple());
+        }
+
+    }
+}
