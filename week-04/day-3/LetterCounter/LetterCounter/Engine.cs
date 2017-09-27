fad43a6dddccbd0e66510ddab4f97111dba3c0e7
bramble100 <arnold.barna@gmail.com>
2017-09-27 16:08:45 +0200

    new(LetterCounter): engine created

---
+++ week-04/day-3/LetterCounter/LetterCounter/Engine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetterCounter
+{
+    public class Engine
+    {
+        public static Dictionary<char, int> Counter(string inputString)
+        {
+            Dictionary<char, int> result = new Dictionary<char, int>();
+
+            foreach (char character in inputString.ToArray<char>())
+            {
+                if (result.ContainsKey(character))
+                {
+                    result[character]++;
+                }
+                else
+                {
+                    result.Add(character, 1);
+                }
+            }
+            return result;
+        }
+    }
+}
