d144c47dcf689ce9f64bc9e40e2b6c2f68bc373c
bramble100 <arnold.barna@gmail.com>
2017-09-27 11:12:27 +0200

    new(MySum): unti test

---
+++ week-04/day-3/SumTest/SumTest/MySum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumTest
+{
+    static class MySum
+    {
+        public static int Sum(List<int> list) => list.Sum();
+    }
+}
---
+++ week-04/day-3/SumTest/SumTest/TestForMySum.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace SumTest
+{
+    [TestFixture]
+    class TestForMySum
+    {
+        [TestCase]
+        public void ListWithOneElement()
+        {
+            Assert.AreEqual(7, MySum.Sum(new List<int>() { 7 }));
+        }
+
+        [TestCase]
+        public void ListWithTwoElement()
+        {
+            Assert.AreEqual(7, MySum.Sum(new List<int>() { 1, 6 }));
+        }
+
+        [TestCase]
+        public void ListWithNegative()
+        {
+            Assert.AreEqual(8, MySum.Sum(new List<int>() { -1, 9 }));
+        }
+
+        [TestCase]
+        public void EmptyList()
+        {
+            Assert.AreEqual(0, MySum.Sum(new List<int>() { }));
+        }
+
+        [TestCase]
+        [Expected]
+        public void Null()
+        {
+            Assert.AreEqual(0, MySum.Sum(null));
+        }
+    }
+}
