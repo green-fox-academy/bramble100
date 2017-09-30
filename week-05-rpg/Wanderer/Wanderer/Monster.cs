51cbf030c52aa4d2992c6d682cf4c49d78f64f93
bramble100 <arnold.barna@gmail.com>
2017-09-30 11:17:49 +0200

    improve(Monster): LevelModifierCalculator added

--- week-05-rpg/Wanderer/Wanderer/Monster.cs
+++ week-05-rpg/Wanderer/Wanderer/Monster.cs
@@ -8,15 +8,29 @@ namespace Wanderer
 {
     class Monster : MovingObject
     {
-        internal Monster(int level, Dice dice) : base(dice)
+        internal Monster(int areaLevel, Dice dice) : base(dice)
         {
+            // the monsters levels come from the number of the area
+            // if its the Xth area, the mosters have lvl X(with 50 % chance) or lvl X + 1(40 %) or lvl X + 2(10 %)
+
+            Level = areaLevel + LevelModifierCalculator();
+
             // Monster Lvl x
             // HP: 2 * x * d6
-            MaximalHealthPoints = CurrentHealthPoints = 2 * level * dice.Roll();
+            MaximalHealthPoints = CurrentHealthPoints = 2 * Level * dice.Roll();
             // DP: x / 2 * d6
-            DefendPoints = level / 2 * dice.Roll() + dice.Roll();
+            DefendPoints = Level / 2 * dice.Roll() + dice.Roll();
             // SP: x* d6
-            StrikePoints = level * dice.Roll();
+            StrikePoints = Level * dice.Roll();
+        }
+
+        private int LevelModifierCalculator()
+        {
+            int random10 = dice.random.Next(10);
+            int modifier = 0; // lower 50 %
+            modifier += random10 > 4 ? 1 : 0; // upper 50%
+            modifier += random10 > 8 ? 1 : 0; // upper 10%
+            return modifier;
         }
     }
 }
