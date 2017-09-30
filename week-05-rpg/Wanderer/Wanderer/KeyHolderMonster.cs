3d336722058663617575a2fb3c961f5de3bb398e
bramble100 <arnold.barna@gmail.com>
2017-09-30 10:53:18 +0200

    improve(KeyHolderMonster): inherited from Monster; constructor

--- week-05-rpg/Wanderer/Wanderer/KeyHolderMonster.cs
+++ week-05-rpg/Wanderer/Wanderer/KeyHolderMonster.cs
@@ -1,6 +1,10 @@
 ﻿namespace Wanderer
 {
-    internal class KeyHolderMonster
+    internal class KeyHolderMonster : Monster
     {
+        // only to register which monster has the key
+        internal KeyHolderMonster(int level, Dice dice) : base(level, dice)
+        {
+        }
     }
 }
