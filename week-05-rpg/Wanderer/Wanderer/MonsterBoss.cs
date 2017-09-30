901c55dc1e34f23ac15e00383c2529b64ee50608
bramble100 <arnold.barna@gmail.com>
2017-09-30 11:03:43 +0200

    improve(MonsterBoss): simplified

--- week-05-rpg/Wanderer/Wanderer/MonsterBoss.cs
+++ week-05-rpg/Wanderer/Wanderer/MonsterBoss.cs
@@ -8,20 +8,15 @@ namespace Wanderer
 {
     class MonsterBoss : Monster
     {
-        internal MonsterBoss(Dice dice) : base(dice)
-        {
-        }
-
-        override protected void Initialize(int level = 1)
+        internal MonsterBoss(int level, Dice dice) : base(dice)
         {
             // Monster Lvl x(if boss)
             // HP: 2 * x * d6(+d6)
             MaximalHealthPoints = CurrentHealthPoints = 2 * level * dice.Roll() + dice.Roll();
             // DP: x / 2 * d6(+d6 / 2)
-            DefendPoints = level / 2 * dice.Roll() + dice.Roll() + dice.Roll()/2;
+            DefendPoints = level / 2 * dice.Roll() + dice.Roll() + dice.Roll() / 2;
             // SP: x* d6(+x)
             StrikePoints = level * dice.Roll() + level;
         }
-
     }
 }
