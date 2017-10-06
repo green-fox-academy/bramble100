9900bbcebc6afba9b6469a943b61ce3adbaeb1b8
bramble100 <arnold.barna@gmail.com>
2017-10-06 16:19:05 +0200

    WhichAreIn: long function but passed the CodeWars tests

---
+++ week-05/WhichAreIn/WhichAreIn/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhichAreIn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static string[] SubStringSearcher(string[] array1, string[] array2)
+        {
+            var result = new HashSet<string>();
+            foreach (string word1 in array1)
+            {
+                foreach(string word2 in array2)
+                {
+                    if (word2.Contains(word1))
+                    {
+                        result.Add(word1);
+                        break;
+                    }
+                }
+            }
+            string[] resultArray = result.ToArray<string>();
+            Array.Sort(resultArray);
+            return resultArray;
+        }
+    }
+}
