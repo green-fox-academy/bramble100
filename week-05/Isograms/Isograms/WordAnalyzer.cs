6068da9df5c0e67337aeaadb544ff45155e23e81
bramble100 <arnold.barna@gmail.com>
2017-10-07 09:16:26 +0200

    new(Isogram): passed the CodeWars tests :)

---
+++ week-05/Isograms/Isograms/WordAnalyzer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Isograms
+{
+    public static class WordAnalyzer
+    {
+        /// <summary>
+        /// Returns true if the given word is isogram.
+        /// </summary>
+        /// <remarks>https://www.codewars.com/kata/54ba84be607a92aa900000f1/</remarks>  
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static bool IsIsogram(string str) => (new HashSet<char>(str.ToLower().ToCharArray())).Count == str.ToCharArray().Length;
+    }
+}
