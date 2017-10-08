ee825421f98b3ec7f3c4583dc25de9a459af9708
bramble100 <arnold.barna@gmail.com>
2017-10-08 22:15:09 +0200

    feat(ShortestWord): the same, one-line solution as seen on CW

---
+++ week-05/ShortestWord/ShortestWord/ShortestWord.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShortestWord
+{
+    public static class ShortestWord
+    {
+        public static int FindShort(string s) => s.Split(' ').Min(t => t.Length);
+    }
+}
---
+++ week-05/ShortestWord/ShortestWordMain/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShortestWord;
+
+namespace ShortestWordMain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "bitcoin take over the world maybe who knows perhaps";
+            Console.WriteLine(ShortestWord.ShortestWord.FindShort(s                ));
+            foreach (var s1 in s.Split(' '))
+            {
+                Console.WriteLine(s1);
+            }
+            Console.ReadKey();
+        }
+    }
+}
