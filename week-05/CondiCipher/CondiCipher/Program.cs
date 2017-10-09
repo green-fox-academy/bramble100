8cf53a9e510e395e4285d2ce1442792c26fdf02c
bramble100 <arnold.barna@gmail.com>
2017-10-09 11:36:13 +0200

    feat(Encode): make shift work with the same given number

---
+++ week-05/CondiCipher/CondiCipher/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CondiCipher
+{
+    /// <summary>
+    /// https://www.codewars.com/kata/condi-cipher/train/csharp
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Encode("crypto", "hello", 10));
+            Console.ReadKey();
+        }
+
+        public static string Encode(string key, string inputMessage, int initShift)
+        {
+            string outputMessage = String.Empty;
+            var keyAlphabet = new List<char>(key.Distinct());
+            keyAlphabet.AddRange("abcdefghijklmnopqrstuvwxyz".Except(key.Distinct()));
+            foreach (var character in inputMessage)
+            {
+                outputMessage += Convert.ToChar('a'+ keyAlphabet.FindIndex(c => c == character));
+            }
+            return outputMessage;
+        }
+
+        public static string Decode(string key, string m, int initShift)
+        {
+            return m;
+        }
+    }
+}
