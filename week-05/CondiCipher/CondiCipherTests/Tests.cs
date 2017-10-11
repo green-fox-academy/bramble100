1692a095452f7ca4e19f8922f00f50ff22b058a0
bramble100 <arnold.barna@gmail.com>
2017-10-11 16:56:44 +0200

    test (CondiCipher): make tests

--- week-05/CondiCipher/CondiCipherTests/Tests.cs
+++ week-05/CondiCipher/CondiCipherTests/Tests.cs
@@ -14,71 +14,71 @@ namespace CondiCipherTests
         [Test]
         public void Encode1()
         {
-            Assert.AreEqual("jx", Kata.Encode("cryptogam", "on", 10));
+            Assert.AreEqual("jx", Kata.Encode("on", "cryptogam", 10));
         }
 
         [Test]
         public void Encode2()
         {
             Assert.AreEqual("cytgmdfmbk", 
-                Kata.Encode("cryptogam", "cryptogram", 0));
+                Kata.Encode("cryptogram", "cryptogam", 0));
         }
 
         [Test]
         public void Encode3()
         {
             Assert.AreEqual("jx wnz xrkvz jnd l ufd vwcz.", 
-                Kata.Encode("cryptogam", "on the first day i got lost.", 10));
+                Kata.Encode("on the first day i got lost.", "cryptogam", 10));
         }
 
         [Test]
         public void Encode4()
         {
             Assert.AreEqual("n ggka cvssb bfe esz omgdyr bqqva", 
-                Kata.Encode("superkey", "i will never eat any grapes again", 4));
+                Kata.Encode("i will never eat any grapes again", "superkey", 4));
         }
 
         [Test]
         public void Decode1()
         {
-            Assert.AreEqual("on", Kata.Decode("cryptogam", "jx", 10));
+            Assert.AreEqual("on", Kata.Decode("jx", "cryptogam", 10));
         }
 
         [Test]
         public void Decode2()
         {
-            Assert.AreEqual("....", Kata.Decode("cryptogam", "....", 10));
+            Assert.AreEqual("....", Kata.Decode("....", "cryptogam", 10));
         }
 
 
         [Test]
         public void Decode3()
         {
-            Assert.AreEqual("sit", Kata.Decode("keykeykeykey", "abc", 10));
+            Assert.AreEqual("sit", Kata.Decode("abc", "keykeykeykey", 10));
         }
 
         [Test]
         public void Decode4()
         {
-            Assert.AreEqual(",sit", Kata.Decode("keykeykeykey", ",abc", 10));
+            Assert.AreEqual(",sit", Kata.Decode(",abc", "keykeykeykey", 10));
         }
 
         [Test]
         public void Decode5()
         {
-            Assert.AreEqual("on the first day i got lost.", Kata.Decode("cryptogam", "jx wnz xrkvz jnd l ufd vwcz.", 10));
+            Assert.AreEqual("on the first day i got lost.", Kata.Decode("jx wnz xrkvz jnd l ufd vwcz.", "cryptogam", 10));
         }
 
         [Test]
         public void Decode6()
         {
-            Assert.AreEqual("i will never eat any grapes again", Kata.Decode("superkey", "n ggka cvssb bfe esz omgdyr bqqva", 30));
+            Assert.AreEqual("i will never eat any grapes again", Kata.Decode("n ggka cvssb bfe esz omgdyr bqqva", "superkey", 30));
         }
 
         [Test]
         public void Decode7()
         {
-            Assert.AreEqual("zva nguhbmmgydx.s,ok se,rmafz vpedgbua", Kata.Decode("nqhbfgmi", "qvf cmnxmdkjfca.p,ab mf,byokf vjhwpcyb", 28));
+            Assert.AreEqual("zva nguhbmmgydx.s,ok se,rmafz vpedgbua", Kata.Decode("qvf cmnxmdkjfca.p,ab mf,byokf vjhwpcyb", "nqhbfgmi", 28));
         }
     }
 }
