7ef16052aac13acaa01f07aeab293000022c64e2
bramble100 <arnold.barna@gmail.com>
2017-10-16 19:16:34 +0200

    feat(Animal): add new abstract class

---
+++ week-07/day-02/Zoo/Zoo/Animal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoo
+{
+    public abstract class Animal
+    {
+        public string name;
+        public int age;
+        public bool isMale;
+        public double weight;
+        public bool canFly;
+
+        public abstract void Greet();
+
+        public abstract void WantChild();
+
+        public abstract void UtterSound();
+
+        public abstract void Move();
+    }
+}
