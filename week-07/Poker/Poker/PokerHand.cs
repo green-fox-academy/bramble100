266f26c478d102893082a331228243ae63a83adc
bramble100 <arnold.barna@gmail.com>
2017-10-16 11:58:56 +0200

    feat(PokerHand): add constructor

--- week-07/Poker/Poker/PokerHand.cs
+++ week-07/Poker/Poker/PokerHand.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace Poker
 {
-    public class PokerHand
+    public class PokerHand : HashSet<PlayingCard>
     {
+        public bool IsValid { get; set; }
+
+        public PokerHand(string inputString)
+        {
+            IsValid = false;
+            string[] playingCards = inputString.Split(' ');
+            foreach (var card in playingCards)
+            {
+                PlayingCard tempCard = new PlayingCard(card);
+                if (tempCard.IsValid)
+                {
+                    Add(tempCard);
+                }
+            }
+
+            IsValid = Count == 5;
+        }
     }
 }
