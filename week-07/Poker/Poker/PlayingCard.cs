609679a3f96a77c4ffb3c74266366a69c030c26f
bramble100 <arnold.barna@gmail.com>
2017-10-16 10:03:10 +0200

    feat(Poker): add PokerHand and PlayingCard class

---
+++ week-07/Poker/Poker/PlayingCard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poker
+{
+    public class PlayingCard
+    {
+        public char Rank { get; }
+        public char Suit { get; }
+        public bool IsValid { get; private set; }
+
+        public PlayingCard(string inputString)
+        {
+            IsValid = (inputString != null);
+        }
+    }
+}
---
+++ week-07/Poker/PokerTest/PokerHandTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Poker;
+
+namespace PokerTest
+{
+    [TestFixture]
+    public class PokerHandTest
+    {
+        PlayingCard playingCard;
+
+        [Test]
+        public void WhenNullInput()
+        {
+            string input = null;
+            playingCard = new PlayingCard(input);
+            Assert.IsFalse(playingCard.IsValid);
+        }
+
+        [Test]
+        public void WhenEmptyInput()
+        {
+            string input = string.Empty;
+            playingCard = new PlayingCard(input);
+            Assert.IsFalse(playingCard.IsValid);
+        }
+    }
+}
