10126f0654bf389323d7a0cfcd30e5a1643d38a6
bramble100 <arnold.barna@gmail.com>
2017-10-17 13:21:50 +0200

    feat: add ElectricGuitar class

---
+++ week-07/day-02/Instruments/Instruments/ElectricGuitar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Instruments
+{
+    class ElectricGuitar : StringedInstrument
+    {
+        protected ElectricGuitar()
+        {
+            NumberOfStrings = 6;
+        }
+
+        public override void Sound()
+        {
+            Console.WriteLine("Twang"); ;
+        }
+    }
+}
