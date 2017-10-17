41f7387fa7d6fc9cad19f033c40dffaf2623e5e2
bramble100 <arnold.barna@gmail.com>
2017-10-17 13:23:37 +0200

    feat: add BassGuitar class

---
+++ week-07/day-02/Instruments/Instruments/BassGuitar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Instruments
+{
+    public class BassGuitar : StringedInstrument
+    {
+        protected BassGuitar()
+        {
+            NumberOfStrings = 4;
+        }
+
+        public override void Sound()
+        {
+            Console.WriteLine("Duum-duum-duum");
+        }
+    }
+}
