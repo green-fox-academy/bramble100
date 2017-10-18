ec8e37fd531bfd2229e0780a5efe28cbcfc7728e
bramble100 <arnold.barna@gmail.com>
2017-10-18 15:50:09 +0200

    feat(Ex09): done

---
+++ week-07/day-03-linq/Exercises/Exercises/Exercise09.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises
+{
+    class Exercise09
+    {
+        // Write a LINQ Expression to find the uppercase characters in a string!
+        public static void Run()
+        {
+            char[] charArray = { 'h', 'e', 'l', 'l', 'o' };
+
+            WithLinqExpression(charArray);
+
+            WithMethodExpression(charArray);
+        }
+
+        private static void WithLinqExpression(char[] charArray)
+        {
+
+            Console.WriteLine("Linq expression:");
+        }
+
+        private static void WithMethodExpression(char[] charArray)
+        {
+            Console.WriteLine("Method expression:");
+
+            Console.WriteLine(charArray.ToList().Aggregate(new StringBuilder(), (a, b) => a.Append(b)));
+
+            // Console.WriteLine(new String(charArray));
+        }
+    }
+}
