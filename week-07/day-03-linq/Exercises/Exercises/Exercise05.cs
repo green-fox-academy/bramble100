3f6898570398dd0e7b736cb296fc17e67197043d
bramble100 <arnold.barna@gmail.com>
2017-10-18 14:43:55 +0200

    refactor(Ex05)

--- week-07/day-03-linq/Exercises/Exercises/Exercise05.cs
+++ week-07/day-03-linq/Exercises/Exercises/Exercise05.cs
@@ -11,23 +11,33 @@ namespace Exercises
         public static void Run()
         {
             int[] n = new int[] { 5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2 };
+            WithLinqExpression(n);
+            WithMethodExpression(n);
+        }
 
+        private static void WithLinqExpression(int[] numbersArray)
+        {
             Console.WriteLine("Linq expression:");
 
-            var numbersLinq = from number in n
-                              where number * number > 20
-                              orderby number
+            var numbersLinq = from number in numbersArray
                               select number;
 
+            numbersLinq
+                .ToList()
+                .ForEach(x => Console.WriteLine($"{x}"));
+        }
+
+        private static void WithMethodExpression(int[] numbersArray)
+        {
             Console.WriteLine("Method expression:");
-            n
+            numbersArray
                 .ToList()
                 .Distinct()
                 .ToList()
                 .OrderBy(number => number)
                 .ToList()
                 .ForEach(number
-                    => Console.WriteLine($"{number}: {n.Count(number2 => number == number2)}"));
+                    => Console.WriteLine($"{number}: {numbersArray.Count(number2 => number == number2)}"));
         }
     }
 }
