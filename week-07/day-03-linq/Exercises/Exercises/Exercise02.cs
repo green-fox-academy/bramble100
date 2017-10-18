000b90d016ec0dfae4e9be69c3c9b4b010f04ae2
bramble100 <arnold.barna@gmail.com>
2017-10-18 11:49:52 +0200

    feat(Ex02): fix bug w/ negative numbers

--- week-07/day-03-linq/Exercises/Exercises/Exercise02.cs
+++ week-07/day-03-linq/Exercises/Exercises/Exercise02.cs
@@ -13,10 +13,13 @@ namespace Exercises
             int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
 
             var evenNumbersLinq = from number in n
-                                  where number % 2 == 1
-                                  select number;
+                                  where number % 2 == 1 || number % 2 == -1
+                                  select Convert.ToDouble(number);
 
             Console.WriteLine($"Linq expression: {evenNumbersLinq.ToList().Average()}");
+            Console.WriteLine($"Method expression: {n.Where(number => number % 2 == 1).Average()}");
+
+            evenNumbersLinq.ToList().ForEach(x => Console.WriteLine(x));
             Console.WriteLine($"Method expression: {n.Where(number => number % 2 == 1).Average()}");
         }
     }
