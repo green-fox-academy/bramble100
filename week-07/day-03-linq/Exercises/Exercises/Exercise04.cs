5b973f7470bb36790236b566a79a14bb442686e7
bramble100 <arnold.barna@gmail.com>
2017-10-18 11:36:01 +0200

    feat(Ex04): done

---
+++ week-07/day-03-linq/Exercises/Exercises/Exercise04.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises
+{
+    class Exercise04
+    {
+        public static void Run()
+        {
+            int[] n = new[] { 3, 9, 2, 8, 6, 5 };
+
+            var squaredNumbersLinq = from number in n
+                                     where number * number > 20
+                                     select number;
+
+            squaredNumbersLinq.ToList().ForEach(number => Console.WriteLine(number));
+            n
+                .ToList()
+                .Where(number => number*number > 20)
+                .ToList()
+                .ForEach(number => Console.WriteLine(number));
+        }
+    }
+
+
+
+}
--- week-07/day-03-linq/Exercises/Exercises/Program.cs
+++ week-07/day-03-linq/Exercises/Exercises/Program.cs
@@ -10,7 +10,7 @@ namespace Exercises
     {
         static void Main(string[] args)
         {
-            Exercise03.Run();
+            Exercise04.Run();
             Console.ReadKey();
         }
     }
