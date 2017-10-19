06fdd36a56a179d147e0356b170dd12e853196ea
bramble100 <arnold.barna@gmail.com>
2017-10-19 10:50:50 +0200

    feat(ParkingLot): add ParkingLot class

---
+++ week-07/day-04-enum-generics/WorkshopEnumGenerics/WorkshopEnumGenerics/ParkingLot.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkshopEnumGenerics
+{
+    public class ParkingLot
+    {
+        int numberOfCars = 256;
+        Random random = new Random();
+        List<Car> cars= new List<Car>();
+
+        public ParkingLot()
+        {
+            return;
+
+            for (int i = 0; i < numberOfCars; i++)
+            {
+                cars[i] = new Car(
+                    (CarColor)random.Next(typeof(CarColor).GetEnumNames().Length),
+                    (CarType)random.Next(typeof(CarType).GetEnumNames().Length));
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Parking Lot:\nCapacity: {numberOfCars}"
+                +$"\nNumber of parking cars: {cars.Count}";
+        }
+    }
+}
