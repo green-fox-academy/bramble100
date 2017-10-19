de9e2b720be016b9fef569fb57a3a10707391806
bramble100 <arnold.barna@gmail.com>
2017-10-19 10:17:00 +0200

    feat(ParkingLot): add Car class

---
+++ week-07/day-04-enum-generics/WorkshopEnumGenerics/WorkshopEnumGenerics/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkshopEnumGenerics
+{
+    public class Car
+    {
+        CarColor color;
+        CarType type;
+        public CarColor Color { get => Color; set => Color = value; }
+        public CarType Type { get => type; set => type = value; }
+
+        public Car(CarColor color, CarType type)
+        {
+            Color = color;
+            Type = type;
+        }
+    }
+}
