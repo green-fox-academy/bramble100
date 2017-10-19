2824b02de5840a8a835d176b990b60fc0e28df55
bramble100 <arnold.barna@gmail.com>
2017-10-19 18:55:51 +0200

    feat(Card): add Value as static dictionary

--- week-07/day-04-enum-generics/WorkshopEnumGenerics/WorkshopEnumGenerics/TwentyPlusOne/Card.cs
+++ week-07/day-04-enum-generics/WorkshopEnumGenerics/WorkshopEnumGenerics/TwentyPlusOne/Card.cs
@@ -12,6 +12,23 @@ namespace WorkshopEnumGenerics.TwentyPlusOne
         private Suit suit;
         private CardColor cardColor;
 
+        public static Dictionary<Rank, int> Value = new Dictionary<Rank, int>
+        {
+            {Rank.Two, 2 },
+            {Rank.Three, 3 },
+            {Rank.Four, 4 },
+            {Rank.Five, 5 },
+            {Rank.Six, 6 },
+            {Rank.Seven, 7 },
+            {Rank.Eight, 8 },
+            {Rank.Nine, 9 },
+            {Rank.Ten, 10 },
+            {Rank.Jack, 10 },
+            {Rank.Queen, 10 },
+            {Rank.King, 10 },
+            {Rank.Ace, 11 }
+        };
+
         public Card(Rank rank, Suit suit)
         {
             this.rank = rank;
