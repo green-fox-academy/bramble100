ad6934e4b0cfd58b23e765f1de1ce437f4b18901
bramble100 <arnold.barna@gmail.com>
2017-10-19 21:24:35 +0200

    feat(Hand): add ToString() method

--- week-07/day-04-enum-generics/WorkshopEnumGenerics/WorkshopEnumGenerics/TwentyPlusOne/Hand.cs
+++ week-07/day-04-enum-generics/WorkshopEnumGenerics/WorkshopEnumGenerics/TwentyPlusOne/Hand.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace WorkshopEnumGenerics.TwentyPlusOne
 {
@@ -46,21 +47,9 @@ namespace WorkshopEnumGenerics.TwentyPlusOne
             }
         }
 
-        public bool IsBlackJack
-        {
-            get
-            {
-                return Value == BLACK_JACK;
-            }
-        }
+        public bool IsBlackJack => Value == BLACK_JACK;
 
-        public bool IsBusted
-        {
-            get
-            {
-                return Value > BLACK_JACK;
-            }
-        }
+        public bool IsBusted => Value > BLACK_JACK;
 
         public void Add(Card card)
         {
@@ -74,6 +63,18 @@ namespace WorkshopEnumGenerics.TwentyPlusOne
             }
             cards.Add(card);
             value = null;
+        }
+
+        public override string ToString()
+        {
+            if (cards.Count == 0)
+            {
+                return "No card in the hand.";
+            }
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine($"The value of the hand is: {Value}");
+            cards.ToList().ForEach(card => stringBuilder.AppendLine(card.ToString()));
+            return stringBuilder.ToString();
         }
     }
 }
