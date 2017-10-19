3096b38326eaaee55086286f7514b8bb3443b6b6
bramble100 <arnold.barna@gmail.com>
2017-10-19 20:33:43 +0200

    feat(Hand): add Value property

--- week-07/day-04-enum-generics/WorkshopEnumGenerics/WorkshopEnumGenerics/TwentyPlusOne/Hand.cs
+++ week-07/day-04-enum-generics/WorkshopEnumGenerics/WorkshopEnumGenerics/TwentyPlusOne/Hand.cs
@@ -1,13 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace WorkshopEnumGenerics.TwentyPlusOne
 {
     public class Hand
     {
         private HashSet<Card> cards = new HashSet<Card>();
+        public const int BLACK_JACK = 21;
+        private int? value;
 
-        public bool IsBlackJack { get; internal set; }
+        public int Value
+        {
+            get
+            {
+                // is there any cards?
+                if (cards.Count == 0)
+                {
+                    value = null;
+                    return 0;
+                }
+
+                // hand needs recalculate?
+                if (value != null)
+                {
+                    return (int)value;
+                }
+
+                // calculate
+                value = cards.ToList().Sum(card => Card.Value[card.Rank]);
+
+                // blackjack or under?
+                if(value <= BLACK_JACK)
+                {
+                    return (int)value;
+                }
+
+                // if contains and ace, reduce value by 10
+                if(cards.ToList().Exists(card => card.Rank == Rank.Ace))
+                {
+                    value -= 10;
+                }
+
+                return (int)value;
+            }
+        }
+
+        public bool IsBlackJack
+        {
+            get
+            {
+                return Value == BLACK_JACK;
+            }
+        }
+
+        public bool IsBusted
+        {
+            get
+            {
+                return Value > BLACK_JACK;
+            }
+        }
 
         public void Add(Card card)
         {
@@ -20,6 +73,7 @@ namespace WorkshopEnumGenerics.TwentyPlusOne
                 throw new ArgumentException($"Hand already contains {card}.");
             }
             cards.Add(card);
+            value = null;
         }
     }
 }
