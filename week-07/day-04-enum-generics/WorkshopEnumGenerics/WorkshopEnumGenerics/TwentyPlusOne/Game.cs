fdcf6ce8e62022acedea3d072e721fafe61e0d3a
bramble100 <arnold.barna@gmail.com>
2017-10-19 19:37:17 +0200

    feat(Game): a few changes
    - reduce number of decks (only one)
    - add IsOver bool
    - add Play() method
    - add FirstDeal() method
    - add Deal() method

--- week-07/day-04-enum-generics/WorkshopEnumGenerics/WorkshopEnumGenerics/TwentyPlusOne/Game.cs
+++ week-07/day-04-enum-generics/WorkshopEnumGenerics/WorkshopEnumGenerics/TwentyPlusOne/Game.cs
@@ -8,28 +8,61 @@ namespace WorkshopEnumGenerics.TwentyPlusOne
 {
     class Game
     {
-        Deck[] decks;
+        Deck deck;
         Hand dealer;
         Hand[] players;
+        public bool IsOver;
 
-        public Game(int numberOfDecks = 1, int numberOfHands = 2)
+        public Game(int numberOfHands = 1)
         {
-            decks = new Deck[numberOfDecks];
+            deck = new Deck();
             dealer = new Hand();
             players = new Hand[numberOfHands];
+            IsOver = false;
 
-            for (int i = 0; i < decks.Length; i++)
+            deck.Shuffle();
+
+            for (int i = 0; i < numberOfHands; i++)
             {
-                decks[i] = new Deck();
-                decks[i].Shuffle();
+                players[i] = new Hand();
+            }
+        }
+
+        public void Play()
+        {
+            FirstDeal();
+        }
+
+        private void FirstDeal()
+        {
+            Deal(dealer, 2);
+            for (int i = 0; i < players.Length; i++)
+            {
+                Deal(i, 2);
+            }
+        }
+
+        private void Deal(int playerIndex, int numberOfCards)
+        {
+            for (int i = 0; i < numberOfCards; i++)
+            {
+                players[playerIndex].Add(deck.PullFirst());
+            }
+        }
+
+        private void Deal(Hand hand, int numberOfCards)
+        {
+            for (int i = 0; i < numberOfCards; i++)
+            {
+                dealer.Add(deck.PullFirst());
             }
         }
 
         public override string ToString()
         {
             return "BlackJack game."
-                + $"\nNumber of decks: {decks.Length}"
-                + $"\nNumber of players: {players.Length}";
+                + $"\nNumber of players: {players.Length}"
+                + $"\nThe game is {(IsOver ? String.Empty : "not ")}over.";
         }
     }
 }
