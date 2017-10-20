4b9691c9f16882ee95f349d70dc8fedb3bc4e4db
bramble100 <arnold.barna@gmail.com>
2017-10-20 10:09:22 +0200

    test(TwentyPlusOne): add tests for hand and card

---
+++ week-07/day-04-enum-generics/WorkshopEnumGenerics/WorkshopEnumGenericsTests/CardTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using WorkshopEnumGenerics.TwentyPlusOne;
+
+namespace WorkshopEnumGenericsTests
+{
+    [TestFixture]
+    public class CardTests
+    {
+        Random random = new Random();
+
+        [Test]
+        public void Constructor()
+        {
+            Rank rank = (Rank)random.Next(typeof(Rank).GetEnumNames().Length);
+            Suit suit = (Suit)random.Next(typeof(Suit).GetEnumNames().Length);
+
+            Card card = new Card(rank, suit);
+
+            Assert.AreEqual(rank, card.Rank);
+            Assert.AreEqual(suit, card.Suit);
+        }
+
+        [Test]
+        public void ToStringMethod()
+        {
+            Rank rank = (Rank)random.Next(typeof(Rank).GetEnumNames().Length);
+            Suit suit = (Suit)random.Next(typeof(Suit).GetEnumNames().Length);
+
+            Card card = new Card(rank, suit);
+
+            Assert.AreEqual($"{rank} of {suit}", card.ToString());
+        }
+    }
+}
---
+++ week-07/day-04-enum-generics/WorkshopEnumGenerics/WorkshopEnumGenericsTests/HandTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using WorkshopEnumGenerics.TwentyPlusOne;
+
+namespace WorkshopEnumGenericsTests
+{
+    [TestFixture]
+    class HandTests
+    {
+        [Test]
+        public void ValueUnderBlackJack()
+        {
+            Hand hand = new Hand();
+            hand.Add(new Card(Rank.Ace, Suit.Clubs));
+
+            Assert.AreEqual(11, hand.Value);
+        }
+
+        [Test]
+        public void IsInGame()
+        {
+            Hand hand = new Hand();
+            hand.Add(new Card(Rank.Ace, Suit.Clubs));
+
+            Assert.True(hand.IsInGame);
+
+        }
+
+        [Test]
+        public void BlackJack()
+        {
+            Hand hand = new Hand();
+            hand.Add(new Card(Rank.Ace, Suit.Clubs));
+            hand.Add(new Card(Rank.Jack, Suit.Clubs));
+
+            Assert.AreEqual(21, hand.Value);
+            Assert.True(hand.IsBlackJack);
+        }
+
+        [Test]
+        public void IsBusted()
+        {
+            Hand hand = new Hand();
+            hand.Add(new Card(Rank.Jack, Suit.Clubs));
+            hand.Add(new Card(Rank.Jack, Suit.Diamonds));
+            hand.Add(new Card(Rank.Jack, Suit.Hearts));
+
+            Assert.AreEqual(30, hand.Value);
+            Assert.True(hand.IsBusted);
+        }
+
+    }
+}
