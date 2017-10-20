e829e6601b4f9fb7df0483b592dced06ac2f72f5
bramble100 <arnold.barna@gmail.com>
2017-10-20 11:57:22 +0200

    test(TwentyPlusOne): add tests for Game (AfterFirstDealGameGoesOn, AfterFirstDealPlayerHasBlackJack)

--- week-07/day-04-enum-generics/WorkshopEnumGenerics/WorkshopEnumGenericsTests/GameTests.cs
+++ week-07/day-04-enum-generics/WorkshopEnumGenerics/WorkshopEnumGenericsTests/GameTests.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using System.Collections.Generic;
 using WorkshopEnumGenerics.TwentyPlusOne;
 
 namespace WorkshopEnumGenericsTests
@@ -6,5 +7,38 @@ namespace WorkshopEnumGenericsTests
     [TestFixture]
     class GameTests
     {
+        [Test]
+        public void AfterFirstDealGameGoesOn()
+        {
+            Game game = new Game();
+            game.dealer.Add(new Card(Rank.Eight, Suit.Hearts));
+            game.dealer.Add(new Card(Rank.Nine, Suit.Hearts));
+
+            game.player.Add(new Card(Rank.Seven, Suit.Clubs));
+            game.player.Add(new Card(Rank.Five, Suit.Clubs));
+
+            Assert.False(game.IsEndedAfterFirstDeal);
+            Assert.False(game.IsOver);
+            Assert.False(game.IsPush);
+            Assert.False(game.IsWonByDealer);
+            Assert.False(game.IsWonByPlayer);
+        }
+
+        [Test]
+        public void AfterFirstDealPlayerHasBlackJack()
+        {
+            Game game = new Game();
+            game.dealer.Add(new Card(Rank.Eight, Suit.Hearts));
+            game.dealer.Add(new Card(Rank.Nine, Suit.Hearts));
+
+            game.player.Add(new Card(Rank.Ace, Suit.Clubs));
+            game.player.Add(new Card(Rank.Jack, Suit.Clubs));
+
+            Assert.True(game.IsEndedAfterFirstDeal);
+            Assert.True(game.IsOver);
+            Assert.False(game.IsPush);
+            Assert.False(game.IsWonByDealer);
+            Assert.True(game.IsWonByPlayer);
+        }
     }
 }
