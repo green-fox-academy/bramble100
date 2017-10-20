1bac50682ae938f71b0bb3ae09792ee2918fd634
bramble100 <arnold.barna@gmail.com>
2017-10-20 11:07:40 +0200

    Revert "test(TwentyPlusOne): add test for low and high ace"
    This reverts commit 743e99c4dfb33f53797b2956f22be3f3ae32c17b.

--- week-07/day-04-enum-generics/WorkshopEnumGenerics/WorkshopEnumGenericsTests/CardTests.cs
+++ week-07/day-04-enum-generics/WorkshopEnumGenerics/WorkshopEnumGenericsTests/CardTests.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using NUnit.Framework;
 using WorkshopEnumGenerics.TwentyPlusOne;
 
@@ -19,40 +23,6 @@ namespace WorkshopEnumGenericsTests
 
             Assert.AreEqual(rank, card.Rank);
             Assert.AreEqual(suit, card.Suit);
-        }
-
-        [Test]
-        public void ValueNoAce()
-        {
-            Rank rank = Rank.Nine;
-            Suit suit = (Suit)random.Next(typeof(Suit).GetEnumNames().Length);
-
-            Card card = new Card(rank, suit);
-
-            Assert.AreEqual(9, card.Value);
-        }
-
-        [Test]
-        public void ValueHighAce()
-        {
-            Rank rank = Rank.Ace;
-            Suit suit = (Suit)random.Next(typeof(Suit).GetEnumNames().Length);
-
-            Card card = new Card(rank, suit);
-            Assert.True(card.IsHighAce);
-            Assert.AreEqual(11, card.Value);
-        }
-
-        [Test]
-        public void ValueLowAce()
-        {
-            Rank rank = Rank.Ace;
-            Suit suit = (Suit)random.Next(typeof(Suit).GetEnumNames().Length);
-
-            Card card = new Card(rank, suit);
-            card.IsHighAce = false;
-            Assert.False(card.IsHighAce);
-            Assert.AreEqual(1, card.Value);
         }
 
         [Test]
