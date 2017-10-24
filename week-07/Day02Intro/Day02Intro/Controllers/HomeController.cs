cec1a3f681967bc1ba67eccef2e2ef14f189e756
bramble100 <arnold.barna@gmail.com>
2017-10-24 13:33:05 +0200

    fix(Day02Intro.Controllers): inherit Controller

--- week-07/Day02Intro/Day02Intro/Controllers/HomeController.cs
+++ week-07/Day02Intro/Day02Intro/Controllers/HomeController.cs
@@ -6,12 +6,12 @@ using System.Threading.Tasks;
 
 namespace Day02Intro.Controllers
 {
-    public class HomeController
+    public class HomeController : Controller
     {
         [Route("hello")]
-        public string Hello()
+        public IActionResult Hello()
         {
-            return "Hello Controller World!";
+            return Content("Hello Controller World!");
         }
     }
 }
---
+++ week-07/Day02Intro/Day02Intro/Models/Greeting.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Day02Intro.Models
+{
+    public class Greeting
+    {
+    }
+}
