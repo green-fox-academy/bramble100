24798a7317e2582a87f5dff4470e81084168fb5d
bramble100 <arnold.barna@gmail.com>
2017-10-24 13:56:00 +0200

    feat(Day02Intro.Controllers): Hello World REST App

---
+++ week-07/Day02Intro/Day02Intro/Controllers/RESTController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Day02Intro.Models;
+
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Day02Intro.Controllers
+{
+    [Route("api")]
+    public class RESTController : Controller
+    {
+        [Route("greeting")]
+        public IActionResult Greeting()
+        {
+            Greeting greeting = new Greeting();
+            greeting.Id = 1234;
+            greeting.Content = "I'm a greeting.";
+            return new JsonResult(greeting);
+        }
+    }
+}
--- week-07/Day02Intro/Day02Intro/Models/Greeting.cs
+++ week-07/Day02Intro/Day02Intro/Models/Greeting.cs
@@ -7,5 +7,7 @@ namespace Day02Intro.Models
 {
     public class Greeting
     {
+        public long Id { get; set; }
+        public string Content { get; set; }
     }
 }
