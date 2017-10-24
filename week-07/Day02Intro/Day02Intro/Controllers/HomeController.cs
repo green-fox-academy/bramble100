f6fdb0cef7ec4528dd7c73353e574e56dd4d6c4f
bramble100 <arnold.barna@gmail.com>
2017-10-24 13:25:53 +0200

    feat(Day02Intro.Controllers): Hello World

---
+++ week-07/Day02Intro/Day02Intro/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Day02Intro.Controllers
+{
+    public class HomeController
+    {
+        [Route("hello")]
+        public string Hello()
+        {
+            return "Hello Controller World!";
+        }
+    }
+}
