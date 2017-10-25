9906937991a7c558716c8797b4d8a093b7bf9f6f
bramble100 <arnold.barna@gmail.com>
2017-10-25 11:44:53 +0200

    feat(Day03BankOfSimba): complete Part III

--- week-07/Day03BankOfSimba/Day03BankOfSimba/Controllers/HomeController.cs
+++ week-07/Day03BankOfSimba/Day03BankOfSimba/Controllers/HomeController.cs
@@ -23,5 +23,43 @@ namespace Day03BankOfSimba.Controllers
             };
             return View(bankAccount);
         }
+
+        [Route("Animals")]
+        public IActionResult ListOfAnimals()
+        {
+            BankAccounts bankAccounts = new BankAccounts();
+            bankAccounts.Accounts.Add(new BankAccount()
+            {
+                Name = "Simba",
+                Balance = 2000,
+                AnimalType = AnimalType.Lion
+            });
+            bankAccounts.Accounts.Add(new BankAccount()
+            {
+                Name = "Zumba",
+                Balance = 1000,
+                AnimalType = AnimalType.Lion
+            });
+            bankAccounts.Accounts.Add(new BankAccount()
+            {
+                Name = "Samba",
+                Balance = 5000,
+                AnimalType = AnimalType.Lion
+            });
+            bankAccounts.Accounts.Add(new BankAccount()
+            {
+                Name = "Mamba",
+                Balance = 4000,
+                AnimalType = AnimalType.Lion
+            });
+            bankAccounts.Accounts.Add(new BankAccount()
+            {
+                Name = "Bamba",
+                Balance = 3000,
+                AnimalType = AnimalType.Lion
+            });
+
+            return View(bankAccounts);
+        }
     }
 }
