1ca058dd5327b8aed5a61bdf8e476d83b99f3931
bramble100 <arnold.barna@gmail.com>
2017-10-28 12:23:34 +0200

    feat(Day06): create form for login with a little CSS

---
+++ week-07/Day06/Practice02/Practice02/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Practice02.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("")]
+        public IActionResult Index()
+        {
+            return RedirectToAction("login");
+        }
+
+        [Route("/login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [Route("/gameselector")]
+        public IActionResult GameSelector()
+        {
+            return View();
+        }
+
+        [Route("/blackjack")]
+        public IActionResult BlackJack()
+        {
+            return View();
+        }
+    }
+}
