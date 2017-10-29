5cf09d5361255da49c13014607a454f63456594e
bramble100 <arnold.barna@gmail.com>
2017-10-29 14:43:57 +0100

    feat(FoxClub): add ViewModels (FoodnDrink class)

---
+++ week-07/FoxClub/FoxClub/ViewModels/FoodnDrink.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoxClub.ViewModels
+{
+    public class FoodnDrink
+    {
+        public string[] Foods => Enum.GetNames(typeof(Models.Food));
+        public string[] Drinks => Enum.GetNames(typeof(Models.Drink));
+    }
+}
