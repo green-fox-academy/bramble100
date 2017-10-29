f3f548125138d20feb5941aa4ed5cc1f7c1b5a42
bramble100 <arnold.barna@gmail.com>
2017-10-29 14:43:01 +0100

    feat(FoxClub): add HelperDictionary

---
+++ week-07/FoxClub/FoxClub/HelperDictionary/HelperDictionary.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FoxClub.Models;
+
+namespace FoxClub.HelperDictionaries
+{
+    public static class HelperDictionary
+    {
+        public static Dictionary<string, Food> StringToFood = new Dictionary<string, Food>()
+        {
+            { "Chicken", Food.Chicken },
+            { "FishermanSoup", Food.FishermanSoup },
+            { "Gulash", Food.Gulash },
+            { "Salad", Food.Salad },
+            { "Venison", Food.Venison }
+        };
+
+        public static Dictionary<string, Drink> StringToDrink = new Dictionary<string, Drink>()
+        {
+            { "Champagne", Drink.Champagne },
+            { "Coke", Drink.Coke },
+            { "Lemonade", Drink.Lemonade },
+            { "Water", Drink.Water },
+            { "Wine", Drink.Wine }
+        };
+
+        public static Dictionary<string, Trick> StringToTrick = new Dictionary<string, Trick>()
+        {
+            { "CatchMouse", Trick.CatchMouse },
+            { "ClimbMountains", Trick.ClimbMountains },
+            { "CodeInCSharp", Trick.CodeInCSharp },
+            { "KillChicken", Trick.KillChicken },
+            { "PrettifyWithCSS", Trick.PrettifyWithCSS },
+            { "SwimWithDolphins", Trick.SwimWithDolphins },
+            { "WriteHTML", Trick.WriteHTML }
+        };
+    }
+}
