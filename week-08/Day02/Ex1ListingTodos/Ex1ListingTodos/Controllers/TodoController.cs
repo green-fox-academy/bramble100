b7ee661183f85329edf7625660429378448c18fd
bramble100 <arnold.barna@gmail.com>
2017-10-31 13:01:42 +0100

    feat(Ex1ListingTodos): create new app

---
+++ week-08/Day02/Ex1ListingTodos/Ex1ListingTodos/Controllers/TodoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Ex1ListingTodos.Controllers
+{
+    public class TodoController : Controller
+    {
+        [Route("")]
+        [Route("/list")]
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
---
+++ week-08/Day02/Ex1ListingTodos/Ex1ListingTodos/Models/Todo.cs
@@ -0,0 +1,10 @@
+﻿namespace Ex1ListingTodos.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsUrgent { get; set; } = false;
+        public bool IsDone { get; set; } = false;
+    }
+}
