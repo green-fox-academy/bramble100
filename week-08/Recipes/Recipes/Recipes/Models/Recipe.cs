388ffb4660f4186d9931357d9961a37fefba9c0a
bramble100 <arnold.barna@gmail.com>
2017-10-31 21:26:11 +0100

    feat(Recipes): create new app

---
+++ week-08/Recipes/Recipes/Recipes/Models/Recipe.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Recipes.Models
+{
+    /// <summary>
+    /// Data for one recipe.
+    /// </summary>
+    public class Recipe
+    {
+        /// <summary>
+        /// The name of the recipe.
+        /// </summary>
+        [Required]
+        public string Name { get; set; }
+        /// <summary>
+        /// The country from which the recipe originates.
+        /// </summary>
+        [Required]
+        public int Cuisine { get; set; }
+        /// <summary>
+        /// The time in minutes that needed for the preparation.
+        /// </summary>
+        [Required]
+        public int PreparationTimeInMinutes { get; set; }
+        /// <summary>
+        /// Shows how hard the recipe is to prepare.
+        /// </summary>
+        public int Level { get; set; } = 0;
+        /// <summary>
+        /// True if the recipe is vegetarian.
+        /// </summary>
+        public bool IsVegetarian { get; set; } = false;
+    }
+}
