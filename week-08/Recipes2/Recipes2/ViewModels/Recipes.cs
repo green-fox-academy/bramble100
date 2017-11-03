174011fb96fce548ae1894d03457402f7d805e56
Arnold Barna <arnold.barna@outlook.com>
2017-11-03 13:35:38 +0100

    feat(Recipes2): make list show number of comments

--- week-08/Recipes2/Recipes2/ViewModels/Recipes.cs
+++ week-08/Recipes2/Recipes2/ViewModels/Recipes.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Recipes2.Entities;
+using Recipes2.Models;
 
 namespace Recipes2.ViewModels
 {
@@ -10,11 +11,30 @@ namespace Recipes2.ViewModels
     {
         public RecipeContext recipeContext;
         public CommentContext commentContext;
+        public int Count => Lines.Count;
+        public List<RecipeLine> Lines { get; set; } = new List<RecipeLine>();
 
         public Recipes(RecipeContext recipeContext, CommentContext commentContext)
         {
             this.recipeContext = recipeContext;
             this.commentContext = commentContext;
+
+            foreach (var recipe in recipeContext.Recipes.OrderBy(r => -r.Votes))
+            {
+                Lines.Add(new RecipeLine()
+                {
+                    Recipe = recipe,
+                    NumberOfComments = 4
+                });
+            }
+        }
+
+        public class RecipeLine
+        {
+            public Recipe Recipe;
+            public int NumberOfComments;
+
+            public bool ThereAreComments => NumberOfComments != 0;
         }
     }
 }
