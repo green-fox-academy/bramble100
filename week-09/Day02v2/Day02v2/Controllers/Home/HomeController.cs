c53ee66163f51c4a29568d16c18c85f66eb33b3a
Arnold Barna <arnold.barna@outlook.com>
2017-11-08 19:26:59 +0100

    feat(Day02v2): close the day

---
+++ week-09/Day02v2/Day02v2/Controllers/Home/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Day02v2.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Day02v2.Controllers.Home
+{
+    [Route("")]
+    public class HomeController : Controller
+    {
+        [HttpPost]
+        //[Route("arrays/{dto?}")]
+        //[Route("arrays")]
+        public IActionResult Index(
+            [FromBody]
+            ArraysDTO dto)
+        {
+            return Json(new { status = "ok" });
+        }
+    }
+}
---
+++ week-09/Day02v2/Day02v2/Models/ArraysDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Day02v2.Models
+{
+    public class ArraysDTO
+    {
+        public string What { get; set; }
+    }
+}
