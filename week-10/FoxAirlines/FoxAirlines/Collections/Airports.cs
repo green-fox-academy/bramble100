43dd4e39eb0c33b6b1104779874bae64fe0c67e6
Arnold Barna <arnold.barna@outlook.com>
2017-11-13 15:20:44 +0100

    feat(FoxAirlines): add Airports

---
+++ week-10/FoxAirlines/FoxAirlines/Collections/Airports.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoxAirlines.Collections
+{
+    public static class Airports
+    {
+        public static readonly HashSet<string> Names = new HashSet<string>()
+        {
+            "Budapest",
+            "Wien",
+            "Bratislava",
+            "Prague",
+            "Madrid",
+            "Berlin",
+            "Copenhagen"
+        };
+
+        public static HashSet<string> OtherAirports(string airport)
+        {
+            var result = Names;
+            result.Remove(airport);
+            return result;
+        }
+    }
+}
