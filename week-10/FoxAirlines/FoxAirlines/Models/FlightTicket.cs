eadae24dfa1dc0979a52e7066e8f9798b1a1c7bf
Arnold Barna <arnold.barna@outlook.com>
2017-11-13 15:21:10 +0100

    feat(FoxAirlines): add FlightTicket

---
+++ week-10/FoxAirlines/FoxAirlines/Models/FlightTicket.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoxAirlines.Models
+{
+    public class FlightTicket
+    {
+        /// <summary>
+        /// Ticket ID.
+        /// </summary>
+        [Key]
+        public int Id { get; set; }
+        /// <summary>
+        /// The airport from where flight travels.
+        /// </summary>
+        [Required]
+        public string Origin { get; private set; }
+        /// <summary>
+        /// The airport to where flight travels.
+        /// </summary>
+        [Required]
+        public string Destination { get; private set; }
+        /// <summary>
+        /// The ID of the buyer.
+        /// </summary>
+        [Required]
+        public int  BuyerId { get; set; }
+    }
+}
