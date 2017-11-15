163c7e2ba46d15ebdd604d5f2ed4520809c9c35e
Arnold Barna <arnold.barna@outlook.com>
2017-11-15 13:49:11 +0100

    feat(LagopusExam): add APIController class and Questions endpoint

---
+++ week-10/LagopusExam/LagopusExam/Controllers/APIController.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LagopusExam.Controllers
+{
+    [Route("")]
+    public class APIController : Controller
+    {
+        [HttpGet]
+        [Route("questions")]
+        public IActionResult Questions()
+        {
+            return Json(new { });
+        }
+    }
+}
