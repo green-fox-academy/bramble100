c437207053c8d4170231a5a74969746163906a13
Arnold Barna <arnold.barna@outlook.com>
2017-11-15 14:16:23 +0100

    style(LagopusExam.Collections): PredefinedQA

--- week-10/LagopusExam/LagopusExam/Collections/PredefinedQA.cs
+++ week-10/LagopusExam/LagopusExam/Collections/PredefinedQA.cs
@@ -9,14 +9,14 @@ namespace LagopusExam.Collections
     {
         public static readonly Dictionary<string, string> QA = new Dictionary<string, string>()
         {
-            {"When did your course start? (yyyy.mm.dd)",  "2017.03.13"},
-            {"What type of dog Barbi has?","Whippet" },
-            {"What is HerrNorbert's favourite color?","Green" },
-            {"How many classes are learning at Green Fox Academy at this moment?","4" },
-            {"How many mentors teach at Green Fox at this moment?","16" },
-            { "What was the name of the first Green Fox class?","Vulpes"},
-            {"How many likes do we have on facebook?","~3300" },
-            {"What is Tojas's horoscope?","Libra" }
+            { "When did your course start? (yyyy.mm.dd)", "2017.03.13"},
+            { "What type of dog Barbi has?", "Whippet" },
+            { "What is HerrNorbert's favourite color?", "Green" },
+            { "How many classes are learning at Green Fox Academy at this moment?", "4" },
+            { "How many mentors teach at Green Fox at this moment?", "16" },
+            { "What was the name of the first Green Fox class?", "Vulpes" },
+            { "How many likes do we have on facebook?", "~3300" },
+            { "What is Tojas's horoscope?","Libra" }
         };
     }
 }
