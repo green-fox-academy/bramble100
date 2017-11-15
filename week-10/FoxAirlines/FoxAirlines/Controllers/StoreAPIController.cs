95509f688172e6335906ba578d427c17c3d49130
Arnold Barna <arnold.barna@outlook.com>
2017-11-15 13:26:33 +0100

    feat(FoxAirlines): add StoreAPIController

---
+++ week-10/FoxAirlines/FoxAirlines/Controllers/StoreAPIController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FoxAirlines.Services;
+using FoxAirlines.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FoxAirlines.Controllers
+{
+    [Route("api")]
+    public class StoreAPIController : Controller
+    {
+        private FoxAirlinesService foxAirlinesService;
+
+        public StoreAPIController(FoxAirlinesService foxAirlinesService) 
+            => this.foxAirlinesService = foxAirlinesService;
+
+        public IActionResult Summary() 
+            => Json(new ViewModels.FlightTicketsOverview(foxAirlinesService.FlightTickets));
+
+        [HttpGet]
+        [Route("new")]
+        public IActionResult BuyTicketForm() 
+            => View(new FlightTicket());
+
+        [HttpPost]
+        [Route("new")]
+        public IActionResult BuyTicketConfirmation(FlightTicket flightTicket)
+        {
+            if (ModelState.IsValid)
+            {
+                foxAirlinesService.AddNewFlightTicket(flightTicket);
+                return View(flightTicket);
+            }
+            return View("BuyTicketForm", flightTicket);
+        }
+    }
+}
