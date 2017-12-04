3475cea4230d5e6ca03c0248e1091092dce8897c
bramble100 <arnold.barna@gmail.com>
2017-12-04 13:21:28 +0100

    create(LagopusExam)

---
+++ week-10/Day-04/LagopusExam/LagopusExam/Services/QuizWithAnswerUnChecked.cs
@@ -0,0 +1,9 @@
+﻿namespace LagopusExam.Services
+{
+    public class QuizWithAnswerUnChecked
+    {
+        public int Id { get; set; }
+        public int QuestionId { get; set; }
+        public string Answer { get; set; }
+    }
+}
