9dc4c7d90e7a22dd357a9f2bbd65950ca5eade06
Arnold Barna <arnold.barna@outlook.com>
2017-12-06 16:46:03 +0100

    style(TodoNet2.Models): remove unnecessary usings from interface ITodoRepository

--- week-12/TodoNet2/TodoNet2/TodoNet2/Interfaces/ITodoRepository.cs
+++ week-12/TodoNet2/TodoNet2/TodoNet2/Interfaces/ITodoRepository.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using TodoNet2.Entities;
+﻿using System.Collections.Generic;
 using TodoNet2.Models;
 
 namespace TodoNet2.Interfaces
