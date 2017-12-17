50000cb6d5c5118330719eddd4f94b0c919dbca6
bramble100 <arnold.barna@gmail.com>
2017-12-17 16:55:08 +0100

    feat(FactoryPattern): create simple study project

---
+++ week-13/FactoryPattern/FactoryPattern/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = (List<int>)Factory.Creator();
+            var list2 = (IEnumerable<int>)Factory.Creator();
+        }
+
+        public static class Factory
+        {
+            public static object Creator()
+            {
+                return new List<int>();
+            }
+        }
+    }
+}
