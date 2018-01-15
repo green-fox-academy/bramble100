0d8edafa5a0b18fef686de574e89f5f7d14999c9
Arnold Barna <arnold.barna@outlook.com>
2018-01-15 09:49:49 +0100

    feat(SecondBiggestNumber): create SecondBiggestNumberMethod()
    - LINQ, method-style

---
+++ week-17-final-exam/FinalExam/SecondBiggestNumber/Filter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SecondBiggestNumber
+{
+    public static class Filter
+    {
+        // 20.
+        // Create a function that takes a list of numbers and returns the second biggest element from it.
+        // https://github.com/greenfox-academy/final-exam-mini-exercises#20
+
+        public static int SecondBiggestNumberMethod(IEnumerable<int> collection) => collection.OrderByDescending(x => x).ElementAt(1);
+    }
+}
