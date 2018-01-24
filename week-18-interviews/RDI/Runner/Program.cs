ab00017d979e5d52441129926e93ba68a85826ce
bramble100 <arnold.barna@gmail.com>
2018-01-24 13:12:26 +0100

    feat(RDI): runner project created

---
+++ week-18-interviews/RDI/Runner/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Runner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fibonacci");
+            Console.WriteLine(String.Join(" ", RDI.Fibonacci.Numbers()));
+            Console.ReadKey();
+        }
+    }
+}
