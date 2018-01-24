00f8e356c09cdeeea4a3032d3b38b394ab4be928
bramble100 <arnold.barna@gmail.com>
2018-01-24 13:11:37 +0100

    feat(Fibonacci): project done

---
+++ week-18-interviews/RDI/RDI/Fibonacci.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RDI
+{
+    public class Fibonacci
+    {
+        // Write a method that displays the first 100 Fibonacci numbers
+        // separated with commas.
+
+        public static IEnumerable<long> Numbers(int numberOfNumbers = 100)
+        {
+            var results = new List<long>() { 0, 1 };
+            for (int i = 2; i < numberOfNumbers + 1; i++)
+            {
+                results.Add(results[i - 1] + results[i - 2]);
+            }
+            return results;
+        }
+    }
+}
